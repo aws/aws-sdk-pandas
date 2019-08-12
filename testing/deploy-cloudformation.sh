a0c9b831b67daf43c8465d1314b4ce8eae36bc81
Igor Tavares <igorborgest@gmail.com>
2019-08-12 20:36:32 -0300

    Remove SAM dependency.

--- testing/deploy-cloudformation.sh
+++ testing/deploy-cloudformation.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-BUCKET=$(jq -r '.Bucket' parameters.json)
 VPC_ID=$(jq -r '.VpcId as $k | "VpcId=\($k)"' parameters.json)
 SUBNET_ID=$(jq -r '.SubnetId as $k | "SubnetId=\($k)"' parameters.json)
 PASSWORD=$(jq -r '.Password as $k | "Password=\($k)"' parameters.json)
 
-rm -rf .aws-sam
-sam build --template sam/template.yaml
-sam package --output-template-file .aws-sam/packaged.yaml --s3-bucket ${BUCKET}
-sam deploy --template-file .aws-sam/packaged.yaml --stack-name aws-data-wrangler-test-arena \
+aws cloudformation deploy \
+--template-file template.yaml \
+--stack-name aws-data-wrangler-test-arena \
 --capabilities CAPABILITY_IAM \
 --parameter-overrides ${VPC_ID} ${SUBNET_ID} ${PASSWORD}
-rm -rf .aws-sam
