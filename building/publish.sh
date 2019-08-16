33b3a20e8094d8562474535bd187d582cf78c4cd
Igor Tavares <igorborgest@gmail.com>
2019-08-16 07:26:08 -0700

    Bumping version to 0.0b31

--- building/publish.sh
+++ building/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ..
-rm -fr build dist .egg requests.egg-info
+rm -fr build dist .egg awswrangler.egg-info
 python setup.py sdist bdist_wheel
 twine upload dist/*
-rm -fr build dist .egg requests.egg-info
+rm -fr build dist .egg awswrangler.egg-info
