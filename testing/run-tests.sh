b849f1ad014fba9ddff328d8061fb57fb55e717e
igorborgest <igorborgest@gmail.com>
2019-11-15 11:36:33 -0300

    Updating all dependencies

--- testing/run-tests.sh
+++ testing/run-tests.sh
@@ -2,9 +2,9 @@
 set -e
 
 cd ..
-pip install --upgrade -e .
 yapf --in-place --recursive setup.py awswrangler testing/test_awswrangler
 mypy awswrangler
 flake8 setup.py awswrangler testing/test_awswrangler
+pip install --upgrade -e .
 pytest --cov=awswrangler testing/test_awswrangler
 cd testing
