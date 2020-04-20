831ff4209228a963328167331e7aa3ed61a8f0f0
igorborgest <igorborgest@gmail.com>
2020-04-20 13:57:59 -0300

    Bumping version to 1.0.4

--- awswrangler/__metadata__.py
+++ awswrangler/__metadata__.py
@@ -7,5 +7,5 @@ Documentation: https://aws-data-wrangler.readthedocs.io/
 
 __title__ = "awswrangler"
 __description__ = "Pandas on AWS."
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __license__ = "Apache License 2.0"
