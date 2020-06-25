949d58b9c8909b4accee77442d66d4c992afd7e5
igorborgest <igorborgest@gmail.com>
2020-06-25 10:26:33 -0300

    Applying mypy.

--- awswrangler/_config.py
+++ awswrangler/_config.py
@@ -1,18 +1,24 @@
 """Configuration file for awswrangler."""
+
 import json
 import logging
 import os
-from collections import namedtuple
-from typing import Dict
+from typing import Dict, Any, Type, NamedTuple
 
-_Config = namedtuple("_Config", ["env_var", "cast", "default"])  # str  # callable
-CONFIG_FILE = ".aws_wrangler.json"
+
+class _Config(NamedTuple):
+    env_var: str
+    cast: Type
+    default: Any
+
+
+CONFIG_FILE: str = ".aws_wrangler.json"
 CONFIG_DEFAULTS: Dict[str, _Config] = {
     "cache": _Config(env_var="AWS_WRANGLER_CACHE", cast=bool, default=False),
     "max_cache_seconds": _Config(env_var="AWS_WRANGLER_MAX_CACHE_SECONDS", cast=int, default=900),
 }
 
-# initialise logger
+# initialize logger
 LOG_FORMAT = '{"time_stamp": "%(asctime)s", "log_level": "%(levelname)s", "log_message": %(message)s}'
 logging.basicConfig(format=LOG_FORMAT, datefmt="%Y-%m-%d %H:%M:%S")
 config_logger = logging.getLogger(__name__)
@@ -43,30 +49,30 @@ class Config:
         >>> config = wr.config.Config()
 
         """
-        if os.path.exists(config_file):
+        if os.path.exists(config_file) is True:
             with open(config_file, "r", encoding="utf-8") as f:
-                json_values = json.load(f)
+                json_values: Dict[str, Any] = json.load(f)
         else:
             json_values = {}
 
-            # consider only attributes found as keys in CONFIG_DEFAULTS
+        # consider only attributes found as keys in CONFIG_DEFAULTS
         for attr, conf in CONFIG_DEFAULTS.items():
             if os.getenv(conf.env_var):
-                value = conf.cast(os.getenv(conf.env_var))
+                value: Any = conf.cast(os.getenv(conf.env_var))
             elif json_values.get(attr):
                 value = json_values.get(attr)
             else:
                 value = conf.default
             super(Config, self).__setattr__(f"_{attr}", value)
 
-    def __getattr__(self, item):
+    def __getattr__(self, item: str) -> Any:
         """Get a configuration item."""
         if item in CONFIG_DEFAULTS.keys():
             return super(Config, self).__getattribute__(f"_{item}")
         else:
             raise AttributeError
 
-    def __setattr__(self, key, value):
+    def __setattr__(self, key: str, value: Any) -> Any:
         """Set a configuration item."""
         if key in CONFIG_DEFAULTS.keys() and isinstance(value, CONFIG_DEFAULTS[key].cast):
             super(Config, self).__setattr__(f"_{key}", value)
@@ -77,4 +83,4 @@ class Config:
                 raise TypeError
 
 
-config = Config()
+config: Config = Config()
