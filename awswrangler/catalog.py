b36b0bc71d01e5b1164e2f1b7824b3327b83e592
igorborgest <igorborgest@gmail.com>
2020-07-12 09:37:16 -0300

    Fix support for boto3 >= 1.14.18 #315

--- awswrangler/catalog.py
+++ awswrangler/catalog.py
@@ -1907,7 +1907,7 @@ def overwrite_table_parameters(
 
 def _get_table_input(
     database: str, table: str, boto3_session: Optional[boto3.Session], catalog_id: Optional[str] = None
-) -> Optional[Dict[str, str]]:
+) -> Optional[Dict[str, Any]]:
     client_glue: boto3.client = _utils.client(service_name="glue", session=boto3_session)
     args: Dict[str, str] = {}
     if catalog_id is not None:
@@ -1918,14 +1918,24 @@ def _get_table_input(
         response: Dict[str, Any] = client_glue.get_table(**args)
     except client_glue.exceptions.EntityNotFoundException:
         return None
-    if "DatabaseName" in response["Table"]:
-        del response["Table"]["DatabaseName"]
-    if "CreateTime" in response["Table"]:
-        del response["Table"]["CreateTime"]
-    if "UpdateTime" in response["Table"]:
-        del response["Table"]["UpdateTime"]
-    if "CreatedBy" in response["Table"]:
-        del response["Table"]["CreatedBy"]
-    if "IsRegisteredWithLakeFormation" in response["Table"]:
-        del response["Table"]["IsRegisteredWithLakeFormation"]
-    return response["Table"]
+
+    table_input: Dict[str, Any] = {}
+    for k, v in response["Table"].items():
+        if k in [
+            "Name",
+            "Description",
+            "Owner",
+            "LastAccessTime",
+            "LastAnalyzedTime",
+            "Retention",
+            "StorageDescriptor",
+            "PartitionKeys",
+            "ViewOriginalText",
+            "ViewExpandedText",
+            "TableType",
+            "Parameters",
+            "TargetTable",
+        ]:
+            table_input[k] = v
+
+    return table_input
