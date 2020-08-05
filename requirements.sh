2b3c7eb40d3e9a59bb2abb92d40ac398237f1c31
Igor Tavares <igorborgest@gmail.com>
2020-08-05 12:21:19 -0300

    Bumping development dependencies versions.

--- requirements.sh
+++ requirements.sh
@@ -2,5 +2,5 @@
 set -ex
 
 pip install --upgrade pip
-pip install --upgrade --upgrade-strategy eager --requirement requirements-dev.txt
-pip install --upgrade --upgrade-strategy eager --editable .
+pip --use-feature=2020-resolver install --upgrade --upgrade-strategy eager --requirement requirements-dev.txt
+pip --use-feature=2020-resolver install --upgrade --upgrade-strategy eager --editable .
