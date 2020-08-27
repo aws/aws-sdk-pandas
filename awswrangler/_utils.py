3466b98a55f110617474059c1742b63ba87351a2
Igor Tavares <igorborgest@gmail.com>
2020-08-27 09:30:52 -0300

    Apply black code style.

--- awswrangler/_utils.py
+++ awswrangler/_utils.py
@@ -74,7 +74,11 @@ def client(service_name: str, session: Optional[boto3.Session] = None) -> boto3.
 def resource(service_name: str, session: Optional[boto3.Session] = None) -> boto3.resource:
     """Create a valid boto3.resource."""
     return ensure_session(session=session).resource(
-        service_name=service_name, use_ssl=True, config=botocore.config.Config(retries={"max_attempts": 10, "mode": "adaptive"}, connect_timeout=10, max_pool_connections=30)
+        service_name=service_name,
+        use_ssl=True,
+        config=botocore.config.Config(
+            retries={"max_attempts": 10, "mode": "adaptive"}, connect_timeout=10, max_pool_connections=30
+        ),
     )
 
 
