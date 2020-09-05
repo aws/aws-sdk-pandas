58daf40a7d70f61da9063a67320c8d60b49841ba
Igor Tavares <igorborgest@gmail.com>
2020-09-05 12:37:41 -0300

    Forcing the pull of the last docker images versions for the lambda layer build.

--- building/lambda/build-docker-images.sh
+++ building/lambda/build-docker-images.sh
@@ -6,6 +6,8 @@ cp ../../requirements-dev.txt .
 
 # Python 3.6
 docker build \
+  --no-cache \
+  --pull \
   --tag awswrangler-build-py36 \
   --build-arg base_image=lambci/lambda:build-python3.6 \
   --build-arg py_dev=python36-devel \
@@ -13,6 +15,8 @@ docker build \
 
 # Python 3.7
 docker build \
+  --no-cache \
+  --pull \
   --tag awswrangler-build-py37 \
   --build-arg base_image=lambci/lambda:build-python3.7 \
   --build-arg py_dev=python37-devel \
@@ -20,6 +24,8 @@ docker build \
 
 # Python 3.8
 docker build \
+  --no-cache \
+  --pull \
   --tag awswrangler-build-py38 \
   --build-arg base_image=lambci/lambda:build-python3.8 \
   --build-arg py_dev=python38-devel \
