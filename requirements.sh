245a353b01612d49b8e4dcc049cf7672558b81ab
Igor Tavares <igorborgest@gmail.com>
2020-09-25 08:36:19 -0300

    Update dev dependencies.

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -ex
 
-pip install --upgrade pip
+pip install --upgrade --upgrade-strategy eager pip wheel
 pip --use-feature=2020-resolver install --upgrade --upgrade-strategy eager --requirement requirements-dev.txt
-pip --use-feature=2020-resolver install --upgrade --upgrade-strategy eager --editable .
