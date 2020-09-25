06be8b4682eddab51f6aafb26eccaa28c964f8ed
Igor Tavares <igorborgest@gmail.com>
2020-09-25 09:55:30 -0300

    Bumping version to 1.9.5

--- awswrangler/__metadata__.py
+++ awswrangler/__metadata__.py
@@ -7,5 +7,5 @@ Documentation: https://aws-data-wrangler.readthedocs.io/
 
 __title__: str = "awswrangler"
 __description__: str = "Pandas on AWS."
-__version__: str = "1.9.4"
+__version__: str = "1.9.5"
 __license__: str = "Apache License 2.0"
