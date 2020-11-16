fe7bfc63193e052d3ab637d7a0d46681eb9346b2
Bryan <kenshin2004528@gmail.com>
2020-11-16 21:42:32 +0800

    add add_column and delete_column function

--- awswrangler/catalog/_add.py
+++ awswrangler/catalog/_add.py
@@ -7,7 +7,12 @@ import boto3
 
 from awswrangler import _utils, exceptions
 from awswrangler._config import apply_configs
-from awswrangler.catalog._definitions import _csv_partition_definition, _parquet_partition_definition
+from awswrangler.catalog._definitions import (
+    _check_column_type,
+    _csv_partition_definition,
+    _parquet_partition_definition,
+    _update_table_definition,
+)
 from awswrangler.catalog._utils import _catalog_id, sanitize_table_name
 
 _logger: logging.Logger = logging.getLogger(__name__)
@@ -157,3 +162,32 @@ def add_parquet_partitions(
         _add_partitions(
             database=database, table=table, boto3_session=boto3_session, inputs=inputs, catalog_id=catalog_id
         )
+
+
+@apply_configs
+def add_column(
+        database: str,
+        table: str,
+        column_name: str,
+        column_type: str = 'string',
+        column_comment: str = None,
+        boto3_session: Optional[boto3.Session] = None,
+        catalog_id: Optional[str] = None,
+) -> None:
+    if _check_column_type(column_type):
+        client_glue: boto3.client = _utils.client(service_name="glue", session=boto3_session)
+        res: Dict[str, Any] = client_glue.get_table(DatabaseName=database, Name=table)
+        table_input: dict = _update_table_definition(res)
+        table_input['StorageDescriptor']['Columns'].append({
+            'Name': column_name,
+            'Type': column_type,
+            'Comment': column_comment
+        })
+        res: Dict[str, Any] = client_glue.update_table(
+            **_catalog_id(catalog_id=catalog_id, DatabaseName=database, TableInput=table_input)
+        )
+        if ("Errors" in res) and res["Errors"]:
+            for error in res["Errors"]:
+                if "ErrorDetail" in error:
+                    if "ErrorCode" in error["ErrorDetail"]:
+                        raise exceptions.ServiceApiError(str(res["Errors"]))
--- awswrangler/catalog/_delete.py
+++ awswrangler/catalog/_delete.py
@@ -1,12 +1,13 @@
 """AWS Glue Catalog Delete Module."""
 
 import logging
-from typing import List, Optional
+from typing import Any, Dict, List, Optional
 
 import boto3
 
-from awswrangler import _utils
+from awswrangler import _utils, exceptions
 from awswrangler._config import apply_configs
+from awswrangler.catalog._definitions import _update_table_definition
 from awswrangler.catalog._get import _get_partitions
 from awswrangler.catalog._utils import _catalog_id
 
@@ -181,3 +182,28 @@ def delete_all_partitions(
         boto3_session=boto3_session,
     )
     return partitions_values
+
+
+@apply_configs
+def delete_column(
+        database: str,
+        table: str,
+        column_name: str,
+        boto3_session: Optional[boto3.Session] = None,
+        catalog_id: Optional[str] = None,
+) -> None:
+    client_glue: boto3.client = _utils.client(service_name="glue", session=boto3_session)
+    res: Dict[str, Any] = client_glue.get_table(DatabaseName=database, Name=table)
+    table_input: dict = _update_table_definition(res)
+    print(table_input)
+    table_input['StorageDescriptor']['Columns'] = \
+        [i for i in table_input['StorageDescriptor']['Columns'] if i['Name'] != column_name]
+    print(table_input)
+    res: Dict[str, Any] = client_glue.update_table(
+        **_catalog_id(catalog_id=catalog_id, DatabaseName=database, TableInput=table_input)
+    )
+    if ("Errors" in res) and res["Errors"]:
+        for error in res["Errors"]:
+            if "ErrorDetail" in error:
+                if "ErrorCode" in error["ErrorDetail"]:
+                    raise exceptions.ServiceApiError(str(res["Errors"]))
