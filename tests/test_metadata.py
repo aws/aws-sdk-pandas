184fc6cb8b1d6e86127419200da9a02479b18954
Igor Tavares <igorborgest@gmail.com>
2020-12-12 18:48:32 -0300

    Bumping version to 2.1.0

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -2,7 +2,7 @@ import awswrangler as wr
 
 
 def test_metadata():
-    assert wr.__version__ == "2.0.1"
+    assert wr.__version__ == "2.1.0"
     assert wr.__title__ == "awswrangler"
     assert wr.__description__ == "Pandas on AWS."
     assert wr.__license__ == "Apache License 2.0"
