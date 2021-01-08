beee7a2dd4cb4d913ea9e60286e291d6574f09ff
Jitesh Soni <sonijite@amazon.com>
2021-01-08 02:27:00 -0800

    Fix logging formatting issues. Use lazy % formatting in logging functions (logging-fstring-interpolation)

--- awswrangler/s3/_merge_upsert_table.py
+++ awswrangler/s3/_merge_upsert_table.py
@@ -7,7 +7,7 @@ import pandas
 
 import awswrangler as wr
 
-logging.basicConfig(level=logging.INFO, format="[%(name)s][%(funcName)s] %(message)s")
+logging.basicConfig(level=logging.INFO)
 logging.getLogger("awswrangler").setLevel(logging.DEBUG)
 logging.getLogger("botocore.credentials").setLevel(logging.CRITICAL)
 _logger: logging.Logger = logging.getLogger(__name__)
@@ -16,7 +16,7 @@ _logger: logging.Logger = logging.getLogger(__name__)
 def _update_existing_table(
     existing_df: pandas.DataFrame, delta_df: pandas.DataFrame, primary_key: List[str], database: str, table: str
 ) -> None:
-    """Perform Update else Insert onto an existing Glue table """
+    """Perform Update else Insert onto an existing Glue table."""
     # Set Index on the pandas dataframe so that join/concat can be made
     existing_df = existing_df.set_index(keys=primary_key, drop=False, verify_integrity=True)
     delta_df = delta_df.set_index(keys=primary_key, drop=False, verify_integrity=True)
@@ -28,13 +28,13 @@ def _update_existing_table(
     path = wr.catalog.get_table_location(database=database, table=table)
     # Write to Glue catalog
     response = wr.s3.to_parquet(df=merged_df, path=path, dataset=True, database=database, table=table, mode="overwrite")
-    _logger.info(f"Successfully Upserted {database}.{table} and got response as {str(response)}")
+    _logger.info("Successfully upserted %s.%s and got response as %s", database, table, str(response))
 
 
 def _is_data_quality_sufficient(
     existing_df: pandas.DataFrame, delta_df: pandas.DataFrame, primary_key: List[str]
 ) -> bool:
-    """Check data quality of existing table and the new delta feed"""
+    """Check data quality of existing table and the new delta feed."""
     error_messages = list()
     # Check for duplicates on the primary key in the existing table
     if sum(pandas.DataFrame(existing_df, columns=primary_key).duplicated()) != 0:
@@ -51,11 +51,12 @@ def _is_data_quality_sufficient(
             f"existing_df is {existing_df.columns} "
         )
     # Return True only if no errors are encountered
+    _logger.info("error_messages %s", str(error_messages))
     return len(error_messages) == 0
 
 
 def merge_upsert_table(delta_df: pandas.DataFrame, database: str, table: str, primary_key: List[str]) -> None:
-    """Perform Upsert (Update else Insert) onto an existing Glue table"""
+    """Perform Upsert (Update else Insert) onto an existing Glue table."""
     # Check if table exists first
     if wr.catalog.does_table_exist(database=database, table=table):
         # Read the existing table into a pandas dataframe
@@ -67,6 +68,6 @@ def merge_upsert_table(delta_df: pandas.DataFrame, database: str, table: str, pr
                 existing_df=existing_df, delta_df=delta_df, primary_key=primary_key, database=database, table=table
             )
     elif wr.catalog.does_table_exist(database=database, table=table) is False:
-        _logger.exception(f"database_name.table_name= {database}.{table} does not exist ")
+        _logger.exception("database= %s and table= %s  does not exist ", database, table)
     else:
         _logger.exception("Reached a unknown logical state")
