dca75f17b26a1e3db14af22e895bb6535872d481
Abdel Jaidi <jaidisido@gmail.com>
2021-02-13 17:34:44 +0000

    Minor - Adding missing @property

--- awswrangler/_config.py
+++ awswrangler/_config.py
@@ -353,6 +353,7 @@ class _Config:  # pylint: disable=too-many-instance-attributes
     def lakeformation_endpoint_url(self, value: Optional[str]) -> None:
         self._set_config_value(key="lakeformation_endpoint_url", value=value)
 
+    @property
     def botocore_config(self) -> botocore.config.Config:
         """Property botocore_config."""
         return cast(Optional[botocore.config.Config], self["botocore_config"])
