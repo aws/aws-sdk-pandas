81a8f7676ed6afea556370804c888cb651f8978f
Maximilian Speicher <maximilian.speicher@tum.de>
2021-02-25 15:50:20 +0100

    Fix sphinx extension setup path

--- docs/source/_ext/copy_tutorials.py
+++ docs/source/_ext/copy_tutorials.py
@@ -3,7 +3,8 @@ from pathlib import Path
 
 
 def setup(app):
-    for f in Path("../tutorials").glob("*.ipynb"):
-        with open(f"source/tutorials/{f.stem}.nblink", "w") as output_file:
+    file_dir = Path(__file__).parent
+    for f in file_dir.joinpath("../../../tutorials").glob("*.ipynb"):
+        with open(file_dir.joinpath(f"../tutorials/{f.stem}.nblink"), "w") as output_file:
             nb_link = {"path": f"../../../tutorials/{f.name}", "extra-media": ["../../../tutorials/_static"]}
             json.dump(nb_link, output_file)
