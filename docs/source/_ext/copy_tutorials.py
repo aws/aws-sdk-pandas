07e81ea2b8bc6ee9f5b1edbd5646de9308b3d7f5
Maximilian Speicher <maximilian.speicher@tum.de>
2021-02-25 13:26:54 +0100

    Linting

--- docs/source/_ext/copy_tutorials.py
+++ docs/source/_ext/copy_tutorials.py
@@ -1,12 +1,9 @@
-from pathlib import Path
 import json
+from pathlib import Path
 
 
 def setup(app):
     for f in Path("../tutorials").glob("*.ipynb"):
         with open(f"source/tutorials/{f.stem}.nblink", "w") as output_file:
-            nb_link = {
-                "path": f"../../../tutorials/{f.name}",
-                "extra-media": ["../../../tutorials/_static"]
-            }
+            nb_link = {"path": f"../../../tutorials/{f.name}", "extra-media": ["../../../tutorials/_static"]}
             json.dump(nb_link, output_file)
