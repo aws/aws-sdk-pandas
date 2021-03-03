6034f760f6681d0b8ef6d19a2c2921710e30d0ae
Abdel Jaidi <jaidisido@gmail.com>
2021-03-03 19:21:30 +0000

    Minor - Fixing _sanitize_name

--- awswrangler/catalog/_utils.py
+++ awswrangler/catalog/_utils.py
@@ -21,8 +21,7 @@ def _catalog_id(catalog_id: Optional[str] = None, **kwargs: Any) -> Dict[str, An
 
 def _sanitize_name(name: str) -> str:
     name = "".join(c for c in unicodedata.normalize("NFD", name) if unicodedata.category(c) != "Mn")  # strip accents
-    name = re.sub("[^A-Za-z0-9_]+", "_", name)  # Replacing non alphanumeric characters by underscore
-    return re.sub("([a-z0-9])([A-Z])", r"\1_\2", name).lower()  # Converting CamelCase to snake_case
+    return re.sub("[^A-Za-z0-9_]+", "_", name).lower()  # Replacing non alphanumeric characters by underscore
 
 
 def _extract_dtypes_from_table_details(response: Dict[str, Any]) -> Dict[str, str]:
