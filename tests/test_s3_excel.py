757c92800c396edf1b6994600b88057e01f686de
Abdel Jaidi <jaidisido@gmail.com>
2021-06-11 13:43:14 +0100

    Parallelizing test

--- tests/test_s3_excel.py
+++ tests/test_s3_excel.py
@@ -8,12 +8,12 @@ import awswrangler as wr
 logging.getLogger("awswrangler").setLevel(logging.DEBUG)
 
 
+@pytest.mark.parametrize("ext", ["xlsx", "xlsm", "xls", "odf"])
 @pytest.mark.parametrize("use_threads", [True, False, 2])
-def test_excel(path, use_threads):
+def test_excel(path, ext, use_threads):
     df = pd.DataFrame({"c0": [1, 2, 3], "c1": ["foo", "boo", "bar"]})
 
-    for ext in ["xlsx", "xlsm", "xls", "odf"]:
-        file_path = f"{path}0.{ext}"
-        wr.s3.to_excel(df, file_path, use_threads=use_threads, index=False)
-        df2 = wr.s3.read_excel(file_path, use_threads=use_threads)
-        assert df.equals(df2)
+    file_path = f"{path}0.{ext}"
+    wr.s3.to_excel(df, file_path, use_threads=use_threads, index=False)
+    df2 = wr.s3.read_excel(file_path, use_threads=use_threads)
+    assert df.equals(df2)
