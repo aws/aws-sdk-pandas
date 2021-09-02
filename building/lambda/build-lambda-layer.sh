8cc40ea0f99fa5707ec4289727a9f6d7190a4baa
Abdel Jaidi <jaidisido@gmail.com>
2021-09-02 14:26:14 +0100

    Minor - Typo

--- building/lambda/build-lambda-layer.sh
+++ building/lambda/build-lambda-layer.sh
@@ -81,7 +81,7 @@ rm -f /aws-data-wrangler/dist/pyarrow_files/pyarrow/libarrow_python.so
 cp -r /aws-data-wrangler/dist/pyarrow_files/pyarrow* python/
 
 find python -wholename "*/tests/*" -type f -delete
-find python -name '*.so*' -type -f -exec strip "{}" \;
+find python -name '*.so*' -type f -exec strip "{}" \;
 
 zip -r9 "${FILENAME}" ./python
 mv "${FILENAME}" dist/
