cb8c69fe0f119673bbbcf9b574957573ef3e2316
jaidisido <jaidisido@gmail.com>
2021-11-27 16:42:58 +0000

    Minor - Adding partition params (#1035)

--- awswrangler/catalog/_definitions.py
+++ awswrangler/catalog/_definitions.py
@@ -71,6 +71,7 @@ def _parquet_partition_definition(
     bucketing_info: Optional[Tuple[List[str], int]],
     compression: Optional[str],
     columns_types: Optional[Dict[str, str]],
+    partitions_parameters: Optional[Dict[str, str]],
 ) -> Dict[str, Any]:
     compressed: bool = compression is not None
     definition: Dict[str, Any] = {
@@ -88,6 +89,7 @@ def _parquet_partition_definition(
             "BucketColumns": [] if bucketing_info is None else bucketing_info[0],
         },
         "Values": values,
+        "Parameters": {} if partitions_parameters is None else partitions_parameters,
     }
     if columns_types is not None:
         definition["StorageDescriptor"]["Columns"] = [
@@ -155,6 +157,7 @@ def _csv_partition_definition(
     serde_library: Optional[str],
     serde_parameters: Optional[Dict[str, str]],
     columns_types: Optional[Dict[str, str]],
+    partitions_parameters: Optional[Dict[str, str]],
 ) -> Dict[str, Any]:
     compressed: bool = compression is not None
     serde_info = {
@@ -175,6 +178,7 @@ def _csv_partition_definition(
             "BucketColumns": [] if bucketing_info is None else bucketing_info[0],
         },
         "Values": values,
+        "Parameters": {} if partitions_parameters is None else partitions_parameters,
     }
     if columns_types is not None:
         definition["StorageDescriptor"]["Columns"] = [
@@ -232,6 +236,7 @@ def _json_partition_definition(
     serde_library: Optional[str],
     serde_parameters: Optional[Dict[str, str]],
     columns_types: Optional[Dict[str, str]],
+    partitions_parameters: Optional[Dict[str, str]],
 ) -> Dict[str, Any]:
     compressed: bool = compression is not None
     serde_info = {
@@ -250,6 +255,7 @@ def _json_partition_definition(
             "BucketColumns": [] if bucketing_info is None else bucketing_info[0],
         },
         "Values": values,
+        "Parameters": {} if partitions_parameters is None else partitions_parameters,
     }
     if columns_types is not None:
         definition["StorageDescriptor"]["Columns"] = [
