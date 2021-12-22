5f3a307ae49fcab9160e6e64fe36e5657bb09497
Dave Bechberger <dbechbe@amazon.com>
2021-12-22 13:59:26 -0900

    Added first draft of potential interface for Neptune

--- awswrangler/neptune/neptune.py
+++ awswrangler/neptune/neptune.py
@@ -98,46 +98,16 @@ def to_graph(
     client: client,
     df: pd.DataFrame
 ) -> None:
-    """Write records of triples stored in a DataFrame into Amazon Neptune.
-
-    Parameters
-    ----------
-    client : neptune.Client
-        instance of the neptune client to use
-    df : pandas.DataFrame
-        Pandas DataFrame https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.html
-
-    Returns
-    -------
-    None
-        None.
-
-    Examples
-    --------
-    Writing to Amazon Neptune 
-
-    >>> import awswrangler as wr
-    >>> client = wr.neptune.Client(host='NEPTUNE-ENDPOINT')
-    >>> wr.neptune.sparql.to_graph(
-    ...     df=df
-    ... )
-    """
-    raise NotImplementedError
-
-def to_graph(
-    client: client,
-    df: pd.DataFrame
-) -> None:
     """Write records stored in a DataFrame into Amazon Neptune.    
     
-    If using property graphs then DataFrames for vertices and edges must be written as separete
-    data frames. DataFrames for vertices must have a ~label column with the label and a ~id column for the vertex id.  
+    If writing to a property graph then DataFrames for vertices and edges must be written as separetly. 
+    DataFrames for vertices must have a ~label column with the label and a ~id column for the vertex id.  
     If the ~id column does not exist, the specified id does not exists, or is empty then a new vertex will be added.  
     If no ~label column exists an exception will be thrown.  
     DataFrames for edges must have a ~id, ~label, ~to, and ~from column.  If the ~id column does not exist, 
     the specified id does not exists, or is empty then a new edge will be added. If no ~label, ~to, or ~from column exists an exception will be thrown.  
 
-    If using RDF then the DataFrame must consist of triples with column names of s, p, and o.
+    If writing to RDF then the DataFrame must consist of triples with column names of s, p, and o.
 
     Parameters
     ----------
