31df062f313e5253798d8ec935af5ec787600efb
Dave Bechberger <dbechbe@amazon.com>
2022-02-28 12:40:49 -0900

    Added GremlinParser to the init file so that it can be unit tested.

--- awswrangler/neptune/__init__.py
+++ awswrangler/neptune/__init__.py
@@ -7,5 +7,14 @@ from awswrangler.neptune.neptune import (
     to_property_graph,
     to_rdf_graph,
 )
+from neptune.gremlin_parser import GremlinParser
 
-__all__ = ["execute_gremlin", "execute_opencypher", "execute_sparql", "to_property_graph", "to_rdf_graph", "connect"]
+__all__ = [
+    "execute_gremlin",
+    "execute_opencypher",
+    "execute_sparql",
+    "to_property_graph",
+    "to_rdf_graph",
+    "connect",
+    "GremlinParser",
+]
