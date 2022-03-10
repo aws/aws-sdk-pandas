8267df15dd3817a48a8d648a5836054682e8c171
Abdel Jaidi <jaidisido@gmail.com>
2022-03-10 11:28:39 +0000

    Tmp fix - Reduce size test shot

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -13,21 +13,11 @@ logger = logging.getLogger("awswrangler")
 logger.setLevel(logging.DEBUG)
 
 
-@pytest.mark.parametrize("length", [-1, 1, 2, 3, 6, 9, 12])
+@pytest.mark.parametrize("length", [-1])
 @pytest.mark.parametrize(
     "seq",
     [
-        (1, 6, 9, 4, 0, 3, 5, 7, 8, 2),
-        (8, 9, 0, 4, 2, 1, 7, 5, 6, 3),
-        (1, 2, 8, 9, 3, 0, 6, 5, 7, 4),
-        (4, 7, 6, 3, 2, 0, 5, 8, 1, 9),
-        (6, 9, 8, 3, 1, 0, 4, 2, 5, 7),
-        (8, 9, 4, 6, 7, 5, 0, 2, 3, 1),
-        (8, 3, 6, 2, 9, 7, 4, 1, 0, 5),
-        (7, 8, 1, 3, 2, 9, 6, 0, 4, 5),
-        (2, 4, 8, 9, 6, 0, 3, 1, 7, 5),
-        (9, 5, 3, 2, 0, 6, 1, 8, 7, 4),
-        (0, 9, 1, 3, 7, 4, 8, 6, 5, 2),
+        (1, 6, 9),
     ],
 )
 def test_read_one_shot(path, seq, length):
