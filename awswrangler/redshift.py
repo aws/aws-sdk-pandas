120e6da3a0defa84fa1327edddefd29d74d31976
Lucas Hanson <lucascchanson@gmail.com>
2022-03-11 03:00:38 -0800

    Support additional sql params in Redshift COPY operation (#1210)
    * adding argument 'sql_copy_extra_params'
    
    * checking var before iterating
    
    Co-authored-by: jaidisido <jaidisido@gmail.com>

--- awswrangler/redshift.py
+++ awswrangler/redshift.py
@@ -130,6 +130,7 @@ def _copy(
     boto3_session: Optional[str] = None,
     schema: Optional[str] = None,
     manifest: Optional[bool] = False,
+    sql_copy_extra_params: Optional[List[str]] = None,
 ) -> None:
     if schema is None:
         table_name: str = f'"{table}"'
@@ -147,6 +148,9 @@ def _copy(
     sql: str = f"COPY {table_name}\nFROM '{path}' {auth_str}\nFORMAT AS PARQUET{ser_json_str}"
     if manifest:
         sql += "\nMANIFEST"
+    if sql_copy_extra_params:
+        for param in sql_copy_extra_params:
+            sql += f"\n{param}"
     _logger.debug("copy query:\n%s", sql)
     cursor.execute(sql)
 
@@ -1199,6 +1203,7 @@ def copy_from_files(  # pylint: disable=too-many-locals,too-many-arguments
     lock: bool = False,
     commit_transaction: bool = True,
     manifest: Optional[bool] = False,
+    sql_copy_extra_params: Optional[List[str]] = None,
     boto3_session: Optional[boto3.Session] = None,
     s3_additional_kwargs: Optional[Dict[str, str]] = None,
 ) -> None:
@@ -1361,6 +1366,7 @@ def copy_from_files(  # pylint: disable=too-many-locals,too-many-arguments
                 aws_session_token=aws_session_token,
                 boto3_session=boto3_session,
                 serialize_to_json=serialize_to_json,
+                sql_copy_extra_params=sql_copy_extra_params,
                 manifest=manifest,
             )
             if table != created_table:  # upsert
