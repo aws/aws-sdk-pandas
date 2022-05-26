5ceaafca4d247e56e85106a538d13079727d0418
lucas hanson <lucascchanson@gmail.com>
2022-05-25 19:37:37 -0700

    refactoring to use pytest

--- .ray/tests/s3_select.py
+++ .ray/tests/s3_select.py
@@ -1,6 +1,7 @@
 import logging
-import sys
 import time
+
+import pytest
 
 import awswrangler as wr
 import ray
@@ -9,25 +10,26 @@ logging.basicConfig(level=logging.INFO, format="[%(name)s][%(funcName)s] %(messa
 logging.getLogger("awswrangler").setLevel(logging.DEBUG)
 logging.getLogger("botocore.credentials").setLevel(logging.CRITICAL)
 
-BENCHMARK_TIME = 60
-start = time.time()
-paths = [
-    "s3://nyc-tlc/trip data/yellow_tripdata_2021-01.parquet",
-    "s3://nyc-tlc/trip data/yellow_tripdata_2021-02.parquet",
-]
-print(f"S3 Select path: {paths}")
-df = wr.s3.select_query(
-    sql="SELECT * FROM s3object",
-    path=paths,
-    input_serialization="Parquet",
-    input_serialization_params={},
-    use_threads=True,
-    scan_range_chunk_size=1024 * 1024 * 32,
-)
-end = time.time()
 
-print(df)
-elapsed_time = end - start
-print(f"Elapsed time: {elapsed_time}")
-if elapsed_time > BENCHMARK_TIME:
-    sys.exit(f"Test has not met benchmark time of {BENCHMARK_TIME} seconds")
+@pytest.mark.repeat(10)  # set for explicit repeats, alternatively pass '--count n' at runtime
+@pytest.mark.parametrize("benchmark_time", [60])
+def test_s3_select(benchmark_time):
+    start = time.time()
+    paths = [
+        "s3://nyc-tlc/trip data/yellow_tripdata_2021-01.parquet",
+        "s3://nyc-tlc/trip data/yellow_tripdata_2021-02.parquet",
+    ]
+    print(f"S3 Select path: {paths}")
+    df = wr.s3.select_query(
+        sql="SELECT * FROM s3object",
+        path=paths,
+        input_serialization="Parquet",
+        input_serialization_params={},
+        use_threads=True,
+        scan_range_chunk_size=1024 * 1024 * 32,
+    )
+    end = time.time()
+
+    elapsed_time = end - start
+    print(f"Benchmark time: {benchmark_time}\nElapsed time: {elapsed_time}")
+    assert elapsed_time < benchmark_time
