5b2514e8c2a73c90fe7db9d5cd2d27423fd3e474
takeknock <takeknock@gmail.com>
2022-06-01 00:55:56 +0900

    Add tests for compression type

--- tests/test_s3_parquet.py
+++ tests/test_s3_parquet.py
@@ -511,6 +511,14 @@ def test_parquet_plain(path) -> None:
     df2 = wr.s3.read_parquet([path_file])
     assert df.equals(df2)
 
+@pytest.mark.parametrize("compression", ["snappy", "gzip", "zstd"])
+def test_parquet_compression(path, compression) -> None:
+    df = pd.DataFrame({"id": [1, 2, 3]}, dtype="Int64")
+    path_file = f"{path}0.parquet"
+    print(path_file)
+    wr.s3.to_parquet(df=df, path=path_file, compression=compression)
+    df2 = wr.s3.read_parquet([path_file])
+    assert df.equals(df2)
 
 @pytest.mark.parametrize("use_threads", [True, False, 2])
 def test_empty_file(path, use_threads):
