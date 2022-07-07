23407909ddb08ce2d4d184e6899357abf624814e
lucas hanson <lucascchanson@gmail.com>
2022-07-07 14:57:43 -0700

    formatting

--- tests/test_opensearch.py
+++ tests/test_opensearch.py
@@ -214,10 +214,13 @@ def test_index_df(client):
     print(response)
     assert response.get("success", 0) == 3
 
+
 def test_index_df_with_array(client):
     response = wr.opensearch.index_df(
         client,
-        df=pd.DataFrame([{"_id": "1", "name": "John", "tags": ["foo", "bar"]}, {"_id": "2", "name": "George", "tags": ["foo"]}]),
+        df=pd.DataFrame(
+            [{"_id": "1", "name": "John", "tags": ["foo", "bar"]}, {"_id": "2", "name": "George", "tags": ["foo"]}]
+        ),
         index="test_index_df1",
     )
     print(response)
