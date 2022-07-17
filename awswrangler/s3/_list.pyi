7133a968d21cc7ccbe274d0eedcd9c2401952f50
Khue Ngoc Dang <khuedang97@gmail.com>
2022-07-17 17:35:49 +0700

    Cleaned up smaller overloads

--- awswrangler/s3/_list.pyi
+++ awswrangler/s3/_list.pyi
@@ -25,34 +25,6 @@ def list_buckets(boto3_session: Optional[boto3.Session] = ...) -> List[str]: ...
 def list_directories(
     path: str,
     chunked: Literal[False],
-    s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None],
-    boto3_session: Optional[boto3.Session],
-) -> List[str]: ...
-@overload
-def list_directories(
-    path: str,
-    chunked: Literal[False],
-    s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None],
-    boto3_session: Optional[boto3.Session] = ...,
-) -> List[str]: ...
-@overload
-def list_directories(
-    path: str,
-    chunked: Literal[False],
-    *,
-    boto3_session: Optional[boto3.Session],
-) -> List[str]: ...
-@overload
-def list_directories(
-    path: str,
-    chunked: Literal[False],
-    s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None] = ...,
-    boto3_session: Optional[boto3.Session] = ...,
-) -> List[str]: ...
-@overload
-def list_directories(
-    path: str,
-    *,
     s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None] = ...,
     boto3_session: Optional[boto3.Session] = ...,
 ) -> List[str]: ...
@@ -63,27 +35,6 @@ def list_directories(
     boto3_session: Optional[boto3.Session] = ...,
     s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None] = ...,
 ) -> List[str]: ...
-@overload
-def list_directories(
-    path: str,
-    chunked: Literal[True],
-    s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None],
-    boto3_session: Optional[boto3.Session],
-) -> Iterator[List[str]]: ...
-@overload
-def list_directories(
-    path: str,
-    chunked: Literal[True],
-    *,
-    boto3_session: Optional[boto3.Session],
-) -> Iterator[List[str]]: ...
-@overload
-def list_directories(
-    path: str,
-    chunked: Literal[True],
-    s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None],
-    boto3_session: Optional[boto3.Session] = ...,
-) -> Iterator[List[str]]: ...
 @overload
 def list_directories(
     path: str,
@@ -95,65 +46,9 @@ def list_directories(
 def list_directories(
     path: str,
     chunked: bool,
-    s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None],
-    boto3_session: Optional[boto3.Session],
-) -> Union[List[str], Iterator[List[str]]]: ...
-@overload
-def list_directories(
-    path: str,
-    chunked: bool,
-    s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None],
-    boto3_session: Optional[boto3.Session] = ...,
-) -> Union[List[str], Iterator[List[str]]]: ...
-@overload
-def list_directories(
-    path: str,
-    chunked: bool,
-    *,
-    boto3_session: Optional[boto3.Session],
-) -> Union[List[str], Iterator[List[str]]]: ...
-@overload
-def list_directories(
-    path: str,
-    chunked: bool,
     s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None] = ...,
     boto3_session: Optional[boto3.Session] = ...,
 ) -> Union[List[str], Iterator[List[str]]]: ...
-@overload
-def list_objects(
-    path: str,
-    *,
-    s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None] = ...,
-    boto3_session: Optional[boto3.Session] = ...,
-) -> List[str]: ...
-@overload
-def list_objects(
-    path: str,
-    *,
-    boto3_session: Optional[boto3.Session] = ...,
-    s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None] = ...,
-) -> List[str]: ...
-@overload
-def list_objects(
-    path: str,
-    chunked: Literal[False],
-    s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None],
-    boto3_session: Optional[boto3.Session],
-) -> List[str]: ...
-@overload
-def list_objects(
-    path: str,
-    chunked: Literal[False],
-    s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None],
-    boto3_session: Optional[boto3.Session] = ...,
-) -> List[str]: ...
-@overload
-def list_objects(
-    path: str,
-    chunked: Literal[False],
-    *,
-    boto3_session: Optional[boto3.Session],
-) -> List[str]: ...
 @overload
 def list_objects(
     path: str,
@@ -164,24 +59,10 @@ def list_objects(
 @overload
 def list_objects(
     path: str,
-    chunked: Literal[True],
-    s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None],
-    boto3_session: Optional[boto3.Session],
-) -> Iterator[List[str]]: ...
-@overload
-def list_objects(
-    path: str,
-    chunked: Literal[True],
-    s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None],
-    boto3_session: Optional[boto3.Session] = ...,
-) -> Iterator[List[str]]: ...
-@overload
-def list_objects(
-    path: str,
-    chunked: Literal[True],
     *,
-    boto3_session: Optional[boto3.Session],
-) -> Iterator[List[str]]: ...
+    s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None] = ...,
+    boto3_session: Optional[boto3.Session] = ...,
+) -> List[str]: ...
 @overload
 def list_objects(
     path: str,
@@ -189,27 +70,6 @@ def list_objects(
     s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None] = ...,
     boto3_session: Optional[boto3.Session] = ...,
 ) -> Iterator[List[str]]: ...
-@overload
-def list_objects(
-    path: str,
-    chunked: bool,
-    s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None],
-    boto3_session: Optional[boto3.Session],
-) -> Union[List[str], Iterator[List[str]]]: ...
-@overload
-def list_objects(
-    path: str,
-    chunked: bool,
-    s3_additional_kwargs: Union[Dict[str, Any], Dict[str, str], None],
-    boto3_session: Optional[boto3.Session] = ...,
-) -> Union[List[str], Iterator[List[str]]]: ...
-@overload
-def list_objects(
-    path: str,
-    chunked: bool,
-    *,
-    boto3_session: Optional[boto3.Session],
-) -> Union[List[str], Iterator[List[str]]]: ...
 @overload
 def list_objects(
     path: str,
