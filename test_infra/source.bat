5f9ec2726b8baa538b221739a4f8b6fbdeb42349
sins111 <113801085+sins111@users.noreply.github.com>
2022-10-05 01:09:27 +0530

    Update source.bat

--- test_infra/source.bat
+++ test_infra/source.bat
@@ -8,6 +8,6 @@ rem (which activates a Python virtualenv on Linux or Mac OS X) work on Windows.
 rem On Windows, this command just runs this batch file (the argument is ignored).
 rem
 rem Now we don't need to document a Windows command for activating a virtualenv.
-
+echo Helo
 echo Executing .venv\Scripts\activate.bat for you
 .venv\Scripts\activate.bat
