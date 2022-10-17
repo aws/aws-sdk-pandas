232ed7112ec8e7891d06fdc35bf8c877805e5223
Abdel Jaidi <jaidisido@gmail.com>
2022-10-17 19:38:20 +0100

    PR feedback: logic to pick most frequent datatypes

--- awswrangler/distributed/ray/modin/_data_types.py
+++ awswrangler/distributed/ray/modin/_data_types.py
@@ -1,5 +1,5 @@
 """Internal (private) Data Types Module."""
-
+from collections import Counter
 from typing import Dict, List, Optional
 
 import modin.pandas as pd
@@ -16,7 +16,7 @@ def pyarrow_types_from_pandas_distributed(
     """Extract the related Pyarrow data types from a pandas DataFrame."""
     func = ray_remote(pyarrow_types_from_pandas)
     block_object_refs = ray.data.from_modin(df).get_internal_block_refs()
-    result = ray_get(
+    list_col_types = ray_get(
         [
             func(
                 df=object_ref,
@@ -27,4 +27,16 @@ def pyarrow_types_from_pandas_distributed(
             for object_ref in block_object_refs
         ]
     )
-    return {k: v for d in result for k, v in d.items()}
+
+    # Dictionaries in list_col_types might not be equal (i.e. different col types in different blocks)
+    # In which case we return the most frequent value for each key
+    # More details here: https://github.com/aws/aws-sdk-pandas/pull/1692
+    keys = set().union(*(d.keys() for d in list_col_types))
+    col_types = {}
+    for key in keys:
+        c = Counter()  # type: ignore
+        for d in list_col_types:
+            if key in d.keys():
+                c[d[key]] += 1
+        col_types[key] = c.most_common(1)[0][0]
+    return col_types
