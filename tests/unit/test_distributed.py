f6eb4d6d00f607aa976a73c7b968dd6b9a4dd47b
Leon Luttenberger <luttenberger.leon@gmail.com>
2022-11-14 15:10:04 -0600

    Refactor unit tests

--- tests/unit/test_distributed.py
+++ tests/unit/test_distributed.py
@@ -26,7 +26,7 @@ def test_engine_lazy_initialization(path: str) -> None:
 
 @pytest.mark.skipif(condition=not is_ray_modin, reason="ray not available")
 def test_engine_explicit_eager_initialization(path: str) -> None:
-    wr.engine.register(EngineEnum.RAY.value)
+    wr.engine.initialize()
 
     assert wr.engine._registry
 
@@ -36,7 +36,7 @@ def test_engine_ray() -> None:
     assert wr.engine.get_installed() == EngineEnum.RAY
     assert wr.engine.get() == EngineEnum.RAY
 
-    wr.engine.register(EngineEnum.RAY.value)
+    wr.engine.initialize(EngineEnum.RAY.value)
 
     assert wr.engine._registry
     assert wr.engine.dispatch_func(_to_parquet).__name__.endswith("distributed")
@@ -44,7 +44,7 @@ def test_engine_ray() -> None:
 
 
 def test_engine_python() -> None:
-    wr.engine.register(EngineEnum.PYTHON.value)
+    wr.engine.initialize(EngineEnum.PYTHON.value)
 
     assert wr.engine.get() == EngineEnum.PYTHON
 
