3edf9af6618023d0c07814ad9d52c2b24444f7fc
Leon Luttenberger <luttenberger.leon@gmail.com>
2022-11-18 13:29:05 -0600

    Change test_distributed import to fixture

--- tests/unit/test_distributed.py
+++ tests/unit/test_distributed.py
@@ -1,10 +1,8 @@
 import logging
+from types import ModuleType
+from typing import Iterable
 
 import pytest
-
-import awswrangler as wr
-from awswrangler._distributed import EngineEnum
-from awswrangler.s3._write_parquet import _to_parquet
 
 from .._utils import is_ray_modin
 
@@ -13,8 +11,16 @@ logging.getLogger("awswrangler").setLevel(logging.DEBUG)
 pytestmark = pytest.mark.distributed
 
 
+@pytest.fixture(scope="function")
+def wr() -> Iterable[ModuleType]:
+    import awswrangler
+
+    yield awswrangler
+    del awswrangler
+
+
 @pytest.mark.skipif(condition=not is_ray_modin, reason="ray not available")
-def test_engine_lazy_initialization(path: str) -> None:
+def test_engine_lazy_initialization(wr: ModuleType, path: str) -> None:
     assert not wr.engine._registry
 
     # any function which dispatches based on engine will
@@ -25,14 +31,17 @@ def test_engine_lazy_initialization(path: str) -> None:
 
 
 @pytest.mark.skipif(condition=not is_ray_modin, reason="ray not available")
-def test_engine_explicit_eager_initialization(path: str) -> None:
+def test_engine_explicit_eager_initialization(wr: ModuleType, path: str) -> None:
     wr.engine.initialize()
 
     assert wr.engine._registry
 
 
 @pytest.mark.skipif(condition=not is_ray_modin, reason="ray not available")
-def test_engine_ray() -> None:
+def test_engine_ray(wr: ModuleType) -> None:
+    from awswrangler._distributed import EngineEnum
+    from awswrangler.s3._write_parquet import _to_parquet
+
     assert wr.engine.get_installed() == EngineEnum.RAY
     assert wr.engine.get() == EngineEnum.RAY
 
@@ -43,7 +52,10 @@ def test_engine_ray() -> None:
     assert not wr.engine.dispatch_func(_to_parquet, "python").__name__.endswith("distributed")
 
 
-def test_engine_python() -> None:
+def test_engine_python(wr: ModuleType) -> None:
+    from awswrangler._distributed import EngineEnum
+    from awswrangler.s3._write_parquet import _to_parquet
+
     wr.engine.initialize(EngineEnum.PYTHON.value)
 
     assert wr.engine.get() == EngineEnum.PYTHON
