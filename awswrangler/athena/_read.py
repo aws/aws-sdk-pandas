1cde34292453337d3b877b04cd85c6226955b4a7
Anton Kukushkin <3997468+kukushking@users.noreply.github.com>
2022-11-28 10:12:21 +0000

    [skip ci] bump version to 2.18.0

--- awswrangler/athena/_read.py
+++ awswrangler/athena/_read.py
@@ -687,11 +687,11 @@ def read_sql_query(
 
     **Related tutorial:**
 
-    - `Amazon Athena <https://aws-sdk-pandas.readthedocs.io/en/2.17.0/
+    - `Amazon Athena <https://aws-sdk-pandas.readthedocs.io/en/2.18.0/
       tutorials/006%20-%20Amazon%20Athena.html>`_
-    - `Athena Cache <https://aws-sdk-pandas.readthedocs.io/en/2.17.0/
+    - `Athena Cache <https://aws-sdk-pandas.readthedocs.io/en/2.18.0/
       tutorials/019%20-%20Athena%20Cache.html>`_
-    - `Global Configurations <https://aws-sdk-pandas.readthedocs.io/en/2.17.0/
+    - `Global Configurations <https://aws-sdk-pandas.readthedocs.io/en/2.18.0/
       tutorials/021%20-%20Global%20Configurations.html>`_
 
     **There are three approaches available through ctas_approach and unload_approach parameters:**
@@ -755,7 +755,7 @@ def read_sql_query(
     /athena.html#Athena.Client.get_query_execution>`_ .
 
     For a practical example check out the
-    `related tutorial <https://aws-sdk-pandas.readthedocs.io/en/2.17.0/
+    `related tutorial <https://aws-sdk-pandas.readthedocs.io/en/2.18.0/
     tutorials/024%20-%20Athena%20Query%20Metadata.html>`_!
 
 
@@ -1001,11 +1001,11 @@ def read_sql_table(
 
     **Related tutorial:**
 
-    - `Amazon Athena <https://aws-sdk-pandas.readthedocs.io/en/2.17.0/
+    - `Amazon Athena <https://aws-sdk-pandas.readthedocs.io/en/2.18.0/
       tutorials/006%20-%20Amazon%20Athena.html>`_
-    - `Athena Cache <https://aws-sdk-pandas.readthedocs.io/en/2.17.0/
+    - `Athena Cache <https://aws-sdk-pandas.readthedocs.io/en/2.18.0/
       tutorials/019%20-%20Athena%20Cache.html>`_
-    - `Global Configurations <https://aws-sdk-pandas.readthedocs.io/en/2.17.0/
+    - `Global Configurations <https://aws-sdk-pandas.readthedocs.io/en/2.18.0/
       tutorials/021%20-%20Global%20Configurations.html>`_
 
     **There are two approaches to be defined through ctas_approach parameter:**
@@ -1050,7 +1050,7 @@ def read_sql_table(
     /athena.html#Athena.Client.get_query_execution>`_ .
 
     For a practical example check out the
-    `related tutorial <https://aws-sdk-pandas.readthedocs.io/en/2.17.0/
+    `related tutorial <https://aws-sdk-pandas.readthedocs.io/en/2.18.0/
     tutorials/024%20-%20Athena%20Query%20Metadata.html>`_!
 
 
