7cb664f53e817d871737ff76900366e576e3316f
Abdel Jaidi <jaidisido@gmail.com>
2022-11-30 15:16:07 +0000

    Switch DynamodDB API call to execute_statement instead of scan

--- awswrangler/dynamodb/__init__.py
+++ awswrangler/dynamodb/__init__.py
@@ -2,7 +2,16 @@
 
 from awswrangler.dynamodb._delete import delete_items
 from awswrangler.dynamodb._read import read_partiql_query
-from awswrangler.dynamodb._utils import get_table
+from awswrangler.dynamodb._utils import execute_statement, get_table
 from awswrangler.dynamodb._write import put_csv, put_df, put_items, put_json
 
-__all__ = ["delete_items", "get_table", "put_csv", "put_df", "put_items", "put_json", "read_partiql_query"]
+__all__ = [
+    "delete_items",
+    "execute_statement",
+    "get_table",
+    "put_csv",
+    "put_df",
+    "put_items",
+    "put_json",
+    "read_partiql_query"
+]
--- awswrangler/dynamodb/_utils.py
+++ awswrangler/dynamodb/_utils.py
@@ -1,11 +1,15 @@
 """Amazon DynamoDB Utils Module (PRIVATE)."""
 
+import logging
 from typing import Any, Dict, List, Mapping, Optional, Union
 
 import boto3
+from botocore.exceptions import ClientError
 
 from awswrangler import _utils, exceptions
 from awswrangler._config import apply_configs
+
+_logger: logging.Logger = logging.getLogger(__name__)
 
 
 @apply_configs
@@ -20,7 +24,7 @@ def get_table(
     table_name : str
         Name of the Amazon DynamoDB table.
     boto3_session : Optional[boto3.Session()]
-        Boto3 Session. The default boto3 Session will be used if boto3_session receive None.
+        Boto3 Session. If None, the default boto3 Session is used.
 
     Returns
     -------
@@ -32,6 +36,87 @@ def get_table(
     dynamodb_table = dynamodb_resource.Table(table_name)
 
     return dynamodb_table
+
+
+def execute_statement(
+    statement: str,
+    parameters: Optional[List[Any]] = None,
+    boto3_session: Optional[boto3.Session] = None,
+) -> List[Optional[Dict[str, Any]]]:
+    """Run a PartiQL statement against a DynamoDB table.
+
+    Parameters
+    ----------
+    statement : str
+        The PartiQL statement.
+    parameters : Optional[List[Any]]
+        The list of PartiQL parameters. These are applied to the statement in the order they are listed.
+    boto3_session : Optional[boto3.Session]
+        Boto3 Session. If None, the default boto3 Session is used.
+
+    Returns
+    -------
+    List[Optional[Dict[str, Any]]]
+        The items from the statement response, if any.
+
+    Examples
+    --------
+    Insert an item
+
+    >>> import awswrangler as wr
+    >>> wr.dynamodb.execute_statement(
+    ...     statement="INSERT INTO movies VALUE {'title': ?, 'year': ?, 'info': ?}",
+    ...     parameters=[title, year, {"plot": plot, "rating": rating}],
+    ... )
+
+    Select items
+
+    >>> wr.dynamodb.execute_statement(
+    ...     statement="SELECT * FROM movies WHERE title=? AND year=?",
+    ...     parameters=[title, year],
+    ... )
+
+    Update items
+
+    >>> wr.dynamodb.execute_statement(
+    ...     statement="UPDATE movies SET info.rating=? WHERE title=? AND year=?",
+    ...     parameters=[rating, title, year],
+    ... )
+
+    Delete items
+
+    >>> wr.dynamodb.execute_statement(
+    ...     statement="DELETE FROM movies WHERE title=? AND year=?",
+    ...     parameters=[title, year],
+    ... )
+    """
+    dynamodb_resource = _utils.resource(service_name="dynamodb", session=boto3_session)
+    kwargs: Dict[str, Union[str, Optional[List[Any]]]] = {"Statement": statement}
+    if parameters:
+        kwargs["Parameters"] = parameters
+    next_token: str = "init_token"  # Dummy token
+    items: List[Optional[Dict[str, Any]]] = []
+
+    while next_token:
+        try:
+            response: Dict[str, Any] = dynamodb_resource.meta.client.execute_statement(**kwargs)
+        except ClientError as err:
+            if err.response["Error"]["Code"] == "ResourceNotFoundException":
+                _logger.error("Couldn't execute PartiQL '%s' because the table does not exist.", statement)
+            else:
+                _logger.error(
+                    "Couldn't execute PartiQL '%s'. %s: %s",
+                    statement,
+                    err.response["Error"]["Code"],
+                    err.response["Error"]["Message"],
+                )
+            raise
+        else:
+            if "Items" in response:
+                items.extend(response["Items"])
+            next_token = response.get("NextToken", None)
+            kwargs["NextToken"] = next_token
+    return items
 
 
 def _validate_items(
