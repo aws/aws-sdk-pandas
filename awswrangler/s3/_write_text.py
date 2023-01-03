41697f915ba6edda9bd3b456ec9df865c142a01a
Leon Luttenberger <luttenberger.leon@gmail.com>
2023-01-03 14:50:25 -0600

    Change sanitize_columns to no longer modify original DF

--- awswrangler/s3/_write_text.py
+++ awswrangler/s3/_write_text.py
@@ -458,7 +458,11 @@ def to_csv(  # pylint: disable=too-many-arguments,too-many-locals,too-many-state
 
     # Sanitize table to respect Athena's standards
     if (sanitize_columns is True) or (database is not None and table is not None):
-        df, dtype, partition_cols = _sanitize(df=df, dtype=dtype, partition_cols=partition_cols)
+        df, dtype, partition_cols = _sanitize(
+            df=df.copy(deep=False),
+            dtype=dtype,
+            partition_cols=partition_cols,
+        )
 
     # Evaluating dtype
     catalog_table_input: Optional[Dict[str, Any]] = None
@@ -893,7 +897,11 @@ def to_json(  # pylint: disable=too-many-arguments,too-many-locals,too-many-stat
 
     # Sanitize table to respect Athena's standards
     if (sanitize_columns is True) or (database is not None and table is not None):
-        df, dtype, partition_cols = _sanitize(df=df, dtype=dtype, partition_cols=partition_cols)
+        df, dtype, partition_cols = _sanitize(
+            df=df.copy(deep=False),
+            dtype=dtype,
+            partition_cols=partition_cols,
+        )
 
     # Evaluating dtype
     catalog_table_input: Optional[Dict[str, Any]] = None
