62165bf263afe666ee3a1d13a122edc729b0d47c
Leon Luttenberger <luttenberger.leon@gmail.com>
2023-01-06 10:09:46 -0600

    Fix conflicts

--- awswrangler/timestream.py
+++ awswrangler/timestream.py
@@ -51,11 +51,8 @@ def _write_batch(
     measure_types: List[str],
     version: int,
     batch: List[Any],
-<<<<<<< HEAD
-=======
     boto3_primitives: _utils.Boto3PrimitivesType,
     measure_name: Optional[str] = None,
->>>>>>> main
 ) -> List[Dict[str, str]]:
     client: boto3.client = _utils.client(
         service_name="timestream-write",
@@ -115,6 +112,7 @@ def _write_df(
     measure_types: List[str],
     version: int,
     boto3_session: Optional[boto3.Session],
+    measure_name: Optional[str] = None,
 ) -> List[Dict[str, str]]:
     batches: List[List[Any]] = _utils.chunkify(lst=_df2list(df=df), max_length=100)
     _logger.debug("len(batches): %s", len(batches))
@@ -128,6 +126,8 @@ def _write_df(
         itertools.repeat(measure_types),
         itertools.repeat(version),
         batches,
+        itertools.repeat(_utils.boto3_to_primitives(boto3_session=boto3_session)),
+        itertools.repeat(measure_name),
     )
 
 
@@ -229,12 +229,8 @@ def write(
     measure_col: Union[str, List[str]],
     dimensions_cols: List[str],
     version: int = 1,
-<<<<<<< HEAD
     use_threads: Union[bool, int] = True,
-=======
-    num_threads: int = 32,
     measure_name: Optional[str] = None,
->>>>>>> main
     boto3_session: Optional[boto3.Session] = None,
 ) -> List[Dict[str, str]]:
     """Store a Pandas DataFrame into a Amazon Timestream table.
@@ -260,18 +256,13 @@ def write(
     version : int
         Version number used for upserts.
         Documentation https://docs.aws.amazon.com/timestream/latest/developerguide/API_WriteRecords.html.
-<<<<<<< HEAD
     use_threads : bool, int
         True to enable concurrent writing, False to disable multiple threads.
         If enabled, os.cpu_count() is used as the number of threads.
         If integer is provided, specified number is used.
-=======
     measure_name : Optional[str]
         Name that represents the data attribute of the time series.
         Overrides ``measure_col`` if specified.
-    num_threads : str
-        Number of thread to be used for concurrent writing.
->>>>>>> main
     boto3_session : boto3.Session(), optional
         Boto3 Session. The default boto3 Session will be used if boto3_session receive None.
 
@@ -307,7 +298,6 @@ def write(
     """
     measure_cols_names = measure_col if isinstance(measure_col, list) else [measure_col]
     _logger.debug("measure_cols_names: %s", measure_cols_names)
-<<<<<<< HEAD
     measure_types: List[str] = _data_types.timestream_type_from_pandas(df.loc[:, measure_cols_names])
     _logger.debug("measure_types: %s", measure_types)
     cols_names: List[str] = [time_col] + measure_cols_names + dimensions_cols
@@ -329,34 +319,10 @@ def write(
                     measure_types=measure_types,
                     version=version,
                     boto3_session=boto3_session,
+                    measure_name=measure_name,
                 )
                 for df in dfs
             ]
-=======
-
-    measure_types: List[str] = [
-        _data_types.timestream_type_from_pandas(df[[measure_col_name]]) for measure_col_name in measure_cols_names
-    ]
-    _logger.debug("measure_types: %s", measure_types)
-    cols_names: List[str] = [time_col] + measure_cols_names + dimensions_cols
-    _logger.debug("cols_names: %s", cols_names)
-    batches: List[List[Any]] = _utils.chunkify(lst=_df2list(df=df[cols_names]), max_length=100)
-    _logger.debug("len(batches): %s", len(batches))
-    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-        res: List[List[Any]] = list(
-            executor.map(
-                _write_batch,
-                itertools.repeat(database),
-                itertools.repeat(table),
-                itertools.repeat(cols_names),
-                itertools.repeat(measure_cols_names),
-                itertools.repeat(measure_types),
-                itertools.repeat(version),
-                batches,
-                itertools.repeat(_utils.boto3_to_primitives(boto3_session=boto3_session)),
-                itertools.repeat(measure_name),
-            )
->>>>>>> main
         )
     )
     return _flatten_list(ray_get(errors))
