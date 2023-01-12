f141c116a079a6dbe50b89e64405ffe0c6f2dadb
kukushking <3997468+kukushking@users.noreply.github.com>
2023-01-12 15:07:17 +0000

    (enhancement) Minor - allow override ParquetWriter args (#1941)
    * Allow override ParquetWriter args
    
    * typing

--- awswrangler/s3/_write_parquet.py
+++ awswrangler/s3/_write_parquet.py
@@ -39,7 +39,7 @@ def _get_file_path(file_counter: int, file_path: str) -> str:
 def _new_writer(
     file_path: str,
     compression: Optional[str],
-    pyarrow_additional_kwargs: Optional[Dict[str, str]],
+    pyarrow_additional_kwargs: Optional[Dict[str, Any]],
     schema: pa.Schema,
     boto3_session: boto3.Session,
     s3_additional_kwargs: Optional[Dict[str, str]],
@@ -55,6 +55,10 @@ def _new_writer(
     if "version" not in pyarrow_additional_kwargs:
         # By default, use version 1.0 logical type set to maximize compatibility
         pyarrow_additional_kwargs["version"] = "1.0"
+    if not pyarrow_additional_kwargs.get("use_dictionary"):
+        pyarrow_additional_kwargs["use_dictionary"] = True
+    if not pyarrow_additional_kwargs.get("write_statistics"):
+        pyarrow_additional_kwargs["write_statistics"] = True
 
     with open_s3_object(
         path=file_path,
@@ -66,8 +70,6 @@ def _new_writer(
         try:
             writer = pyarrow.parquet.ParquetWriter(
                 where=f,
-                write_statistics=True,
-                use_dictionary=True,
                 compression="NONE" if compression is None else compression,
                 schema=schema,
                 **pyarrow_additional_kwargs,
