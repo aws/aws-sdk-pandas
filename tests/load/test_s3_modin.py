a3768f572210799c50878a4174860d966747da93
Leon Luttenberger <luttenberger.leon@gmail.com>
2023-01-23 11:55:32 -0600

    Add modin tests

---
+++ tests/load/test_s3_modin.py
@@ -0,0 +1,48 @@
+from typing import List, Optional, Tuple
+
+import modin.pandas as pd
+import pytest
+
+import awswrangler as wr
+
+from .._utils import ExecutionTimer
+
+
+@pytest.fixture(scope="function")
+def df_s() -> pd.DataFrame:
+    # Data frame with 100000 rows
+    return pd.read_parquet("s3://ursa-labs-taxi-data/2010/02/data.parquet")
+
+
+@pytest.mark.parametrize("benchmark_time", [40])
+def test_modin_s3_read_parquet_simple(benchmark_time: float, request: pytest.FixtureRequest) -> None:
+    path = "s3://ursa-labs-taxi-data/2018/"
+    with ExecutionTimer(request, data_paths=path) as timer:
+        pd.read_parquet(path)
+
+    assert timer.elapsed_time < benchmark_time
+
+
+@pytest.mark.parametrize("benchmark_time", [5])
+def test_modin_s3_write_parquet_simple(
+    df_s: pd.DataFrame, path: str, benchmark_time: float, request: pytest.FixtureRequest
+) -> None:
+    with ExecutionTimer(request, data_paths=path) as timer:
+        df_s.to_parquet(path)
+
+    assert timer.elapsed_time < benchmark_time
+
+
+@pytest.mark.parametrize("benchmark_time", [30])
+@pytest.mark.parametrize("partition_cols", [None, ["payment_type"], ["payment_type", "passenger_count"]])
+def test_modin_s3_write_parquet_dataset(
+    df_s: pd.DataFrame,
+    path: str,
+    partition_cols: Optional[List[str]],
+    benchmark_time: float,
+    request: pytest.FixtureRequest,
+) -> None:
+    with ExecutionTimer(request, data_paths=path) as timer:
+        df_s.to_parquet(path, partition_cols=partition_cols)
+
+    assert timer.elapsed_time < benchmark_time
