c4e97c61cd5069cff6a869a41405cdba6d31ccfa
Leon Luttenberger <luttenberger.leon@gmail.com>
2023-02-03 15:09:16 -0600

    Add type generic to executor.map

--- awswrangler/distributed/ray/_pool.py
+++ awswrangler/distributed/ray/_pool.py
@@ -2,19 +2,21 @@
 
 import itertools
 import logging
-from typing import TYPE_CHECKING, Any, Callable, List, Optional
+from typing import TYPE_CHECKING, Any, Callable, List, Optional, TypeVar
 
 if TYPE_CHECKING:
     from botocore.client import BaseClient
 
 _logger: logging.Logger = logging.getLogger(__name__)
 
+MapOutputType = TypeVar("MapOutputType")
+
 
 class _RayPoolExecutor:
     def __init__(self) -> None:
         pass
 
-    def map(self, func: Callable[..., Any], _: Optional["BaseClient"], *args: Any) -> List[Any]:
+    def map(self, func: Callable[..., MapOutputType], _: Optional["BaseClient"], *args: Any) -> List[MapOutputType]:
         """Map func and return ray futures."""
         _logger.debug("Ray map: %s", func)
         # Discard boto3 client
