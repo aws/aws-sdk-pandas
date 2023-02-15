9452fe4d047639cd907954985ba1a7339121c7e6
Leon Luttenberger <LeonLuttenberger@users.noreply.github.com>
2023-02-15 09:30:24 -0600

    Distributed: Add support for JSON PyArrow data source (#2019)

--- awswrangler/distributed/ray/datasources/__init__.py
+++ awswrangler/distributed/ray/datasources/__init__.py
@@ -1,6 +1,7 @@
 """Ray Datasources Module."""
 
 from awswrangler.distributed.ray.datasources.arrow_csv_datasource import ArrowCSVDatasource
+from awswrangler.distributed.ray.datasources.arrow_json_datasource import ArrowJSONDatasource
 from awswrangler.distributed.ray.datasources.arrow_parquet_datasource import ArrowParquetDatasource
 from awswrangler.distributed.ray.datasources.pandas_file_based_datasource import UserProvidedKeyBlockWritePathProvider
 from awswrangler.distributed.ray.datasources.pandas_text_datasource import (
@@ -12,6 +13,7 @@ from awswrangler.distributed.ray.datasources.pandas_text_datasource import (
 
 __all__ = [
     "ArrowCSVDatasource",
+    "ArrowJSONDatasource",
     "ArrowParquetDatasource",
     "PandasCSVDataSource",
     "PandasFWFDataSource",
