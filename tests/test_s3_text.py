1b3edd2bf3c25b17d2fcca4d9528116ad2cafbe3
kukushking <kukushkin.anton@gmail.com>
2023-02-23 10:50:25 +0000

    Fix SSE defaults (#2049)

--- tests/test_s3_text.py
+++ tests/test_s3_text.py
@@ -315,7 +315,7 @@ def test_csv_additional_kwargs(path, kms_key_id, s3_additional_kwargs, use_threa
     assert df.equals(wr.s3.read_csv([path]))
     desc = wr.s3.describe_objects([path])[path]
     if s3_additional_kwargs is None:
-        assert desc.get("ServerSideEncryption") is None
+        assert desc.get("ServerSideEncryption") == "AES256"
     elif s3_additional_kwargs["ServerSideEncryption"] == "aws:kms":
         assert desc.get("ServerSideEncryption") == "aws:kms"
     elif s3_additional_kwargs["ServerSideEncryption"] == "AES256":
