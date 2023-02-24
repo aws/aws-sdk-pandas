971a96dd5f8038e1bf196d2b3a032020ccac0799
Abdel Jaidi <jaidisido@gmail.com>
2023-02-24 11:26:56 +0000

    docs: fix contributing guide

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -218,7 +218,8 @@ def test_additional_kwargs(path, kms_key_id, s3_additional_kwargs, use_threads):
         assert s3obj.read() == "foo"
     desc = wr.s3.describe_objects([path])[path]
     if s3_additional_kwargs is None:
-        assert desc.get("ServerSideEncryption") is None
+        # S3 default encryption
+        assert desc.get("ServerSideEncryption") == "AES256"
     elif s3_additional_kwargs["ServerSideEncryption"] == "aws:kms":
         assert desc.get("ServerSideEncryption") == "aws:kms"
     elif s3_additional_kwargs["ServerSideEncryption"] == "AES256":
