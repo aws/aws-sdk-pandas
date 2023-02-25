7a6fab34cf2a73533d4d3f96896c9bff8fd9d002
Leon Luttenberger <luttenberger.leon@gmail.com>
2023-02-24 18:41:56 -0600

    fix test_athena_wait_delay_config

--- tests/test_config.py
+++ tests/test_config.py
@@ -233,6 +233,8 @@ def test_athena_wait_delay_config(wr: ModuleType, glue_database: str, polling_de
 
         polling_delay = _QUERY_WAIT_POLLING_DELAY
 
+        wr.config.reset("athena_query_wait_polling_delay")
+
     with patch("awswrangler.athena._utils.wait_query", wraps=wr.athena.wait_query) as mock_wait_query:
         wr.athena.read_sql_query("SELECT 1 as col0", database=glue_database)
 
