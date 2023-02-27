25decbafb4d3b2f154e4d0ae79916e23ad532b12
Anton Kukushkin <kukushkin.anton@gmail.com>
2023-02-27 13:55:51 +0000

    Add @Experimental and @Deprecated annotations.

---
+++ awswrangler/annotations.py
@@ -0,0 +1,68 @@
+"""Annotations Module."""
+
+import logging
+import warnings
+from functools import partial, wraps
+from typing import Any, Callable, Optional, TypeVar, cast
+
+from awswrangler._config import _insert_str
+
+FunctionType = TypeVar("FunctionType", bound=Callable[..., Any])
+
+
+class SDKPandasDeprecatedWarning(Warning):
+    """Deprecated Warning"""
+
+    pass
+
+
+class SDKPandasExperimentalWarning(Warning):
+    """Experiental Warning"""
+
+    pass
+
+
+def _inject_note(
+    doc: Optional[str],
+    message: str,
+) -> Optional[str]:
+    if not doc or "\n    Parameters" not in doc:
+        return doc
+    note: str = f"\n\n    Note\n    ----\n    {message}\n\n"
+    return _insert_str(text=doc, token="", insert=note)
+
+
+def warn_message(
+    message: str,
+    warning_class: Warning,
+    stacklevel: int = 2,
+) -> Callable[[FunctionType], FunctionType]:
+    def decorator(func: FunctionType) -> FunctionType:
+        @wraps(func)
+        def inner(*args: Any, **kwargs: Any) -> Any:
+            warnings.warn(f"`{func.__name__}`: {message}", warning_class, stacklevel=stacklevel)
+
+            return func(*args, **kwargs)
+
+        inner.__doc__ = _inject_note(
+            doc=func.__doc__,
+            message=message,
+        )
+
+        return cast(FunctionType, inner)
+
+    return decorator
+
+
+Deprecated = partial(
+    warn_message,
+    "This API is deprecated and will be removed e in future AWS SDK for Pandas releases. ",
+    SDKPandasDeprecatedWarning,
+)
+
+
+Experimental = partial(
+    warn_message,
+    "This API is experimental and may change in future AWS SDK for Pandas releases. ",
+    SDKPandasExperimentalWarning,
+)
