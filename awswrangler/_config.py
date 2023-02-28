5dab07f52f120e1739ce5de321c82d1a2f8cc779
Leon Luttenberger <luttenberger.leon@gmail.com>
2023-02-28 11:05:06 -0600

    Change suppress_warnings to bool with default value

--- awswrangler/_config.py
+++ awswrangler/_config.py
@@ -44,7 +44,7 @@ _CONFIG_ARGS: Dict[str, _ConfigArg] = {
     "s3_block_size": _ConfigArg(dtype=int, nullable=False, enforced=True),
     "workgroup": _ConfigArg(dtype=str, nullable=False, enforced=True),
     "chunksize": _ConfigArg(dtype=int, nullable=False, enforced=True),
-    "suppress_warnings": _ConfigArg(dtype=str, nullable=True),
+    "suppress_warnings": _ConfigArg(dtype=bool, nullable=False, default=False, loaded=True),
     # Endpoints URLs
     "s3_endpoint_url": _ConfigArg(dtype=str, nullable=True, enforced=True, loaded=True),
     "athena_endpoint_url": _ConfigArg(dtype=str, nullable=True, enforced=True, loaded=True),
@@ -81,7 +81,6 @@ class _Config:  # pylint: disable=too-many-instance-attributes,too-many-public-m
     def __init__(self) -> None:
         self._loaded_values: Dict[str, Optional[_ConfigValueType]] = {}
         name: str
-        self.suppress_warnings = None
         self.s3_endpoint_url = None
         self.athena_endpoint_url = None
         self.sts_endpoint_url = None
@@ -396,12 +395,12 @@ class _Config:  # pylint: disable=too-many-instance-attributes,too-many-public-m
         self._set_config_value(key="chunksize", value=value)
 
     @property
-    def suppress_warnings(self) -> Optional[str]:
+    def suppress_warnings(self) -> bool:
         """Property suppress_warnings."""
-        return cast(Optional[str], self["suppress_warnings"])
+        return cast(bool, self["suppress_warnings"])
 
     @suppress_warnings.setter
-    def suppress_warnings(self, value: Optional[str]) -> None:
+    def suppress_warnings(self, value: bool) -> None:
         self._set_config_value(key="suppress_warnings", value=value)
 
     @property
