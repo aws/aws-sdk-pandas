7df0d39cfb9abfdbd5cddd01dcbafca872b06cff
Leon Luttenberger <LeonLuttenberger@users.noreply.github.com>
2023-03-21 11:46:47 -0500

    Prepare 2.20.1 (#2138)

--- awswrangler/__metadata__.py
+++ awswrangler/__metadata__.py
@@ -7,5 +7,5 @@ Documentation: https://aws-sdk-pandas.readthedocs.io/
 
 __title__: str = "awswrangler"
 __description__: str = "Pandas on AWS."
-__version__: str = "2.20.0"
+__version__: str = "2.20.1"
 __license__: str = "Apache License 2.0"
