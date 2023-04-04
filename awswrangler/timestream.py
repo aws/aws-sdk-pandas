8539701d393507b276f9f962c5fd5116b9512315
Leon Luttenberger <luttenberger.leon@gmail.com>
2023-04-04 11:33:37 -0500

    Improve logging for timestream

--- awswrangler/timestream.py
+++ awswrangler/timestream.py
@@ -177,7 +177,7 @@ def _write_df(
         botocore_config=Config(read_timeout=20, max_pool_connections=5000, retries={"max_attempts": 10}),
     )
     batches: List[List[Any]] = _utils.chunkify(lst=_df2list(df=df), max_length=100)
-    _logger.debug("len(batches): %s", len(batches))
+    _logger.debug("Writing %d nbatches of data", len(batches))
     return executor.map(
         _write_batch,  # type: ignore[arg-type]
         timestream_client,
@@ -401,7 +401,9 @@ def write(
     common_attributes = _sanitize_common_attributes(common_attributes, version, measure_name)
 
     _logger.debug(
-        "common_attributes: %s\n, cols_names: %s\n, measure_types: %s",
+        "Writing to Timestream table %s in database %s\ncommon_attributes: %s\n, cols_names: %s\n, measure_types: %s",
+        table,
+        database,
         common_attributes,
         cols_names,
         measure_types,
@@ -420,7 +422,7 @@ def write(
         raise exceptions.InvalidArgumentCombination(
             "At least one of `time_col`, `measure_col` or `dimensions_cols` must be specified."
         )
-    _logger.debug("len(dfs): %s", len(dfs))
+    _logger.debug("Writing %d dataframes to Timestrea table", len(dfs))
 
     executor: _BaseExecutor = _get_executor(use_threads=use_threads)
     errors = list(
@@ -558,7 +560,8 @@ def create_database(
     >>> arn = wr.timestream.create_database("MyDatabase")
 
     """
-    client = _utils.client(service_name="timestream-write", session=boto3_session)
+    _logger.info("Creating Timestream database %s", database)
+    client = _utils.client(service_name="timestream-wxrite", session=boto3_session)
     args: Dict[str, Any] = {"DatabaseName": database}
     if kms_key_id is not None:
         args["KmsKeyId"] = kms_key_id
@@ -602,6 +605,7 @@ def delete_database(
     >>> arn = wr.timestream.delete_database("MyDatabase")
 
     """
+    _logger.info("Deleting Timestream database %s", database)
     client = _utils.client(service_name="timestream-write", session=boto3_session)
     client.delete_database(DatabaseName=database)
 
@@ -661,6 +665,7 @@ def create_table(
     ... )
 
     """
+    _logger.info("Creating Timestream table %s in database %s", table, database)
     client = _utils.client(service_name="timestream-write", session=boto3_session)
     timestream_additional_kwargs = {} if timestream_additional_kwargs is None else timestream_additional_kwargs
     args: Dict[str, Any] = {
@@ -715,6 +720,7 @@ def delete_table(
     >>> arn = wr.timestream.delete_table("MyDatabase", "MyTable")
 
     """
+    _logger.info("Deleting Timestream table %s in database %s", table, database)
     client = _utils.client(service_name="timestream-write", session=boto3_session)
     client.delete_table(DatabaseName=database, TableName=table)
 
