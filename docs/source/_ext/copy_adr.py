548d86c7f3e48422ca79a01fc7ddf5323df91308
Leon Luttenberger <luttenberger.leon@gmail.com>
2023-04-19 13:54:59 -0500

    Move adr directory

--- docs/source/_ext/copy_adr.py
+++ docs/source/_ext/copy_adr.py
@@ -5,7 +5,7 @@ from pathlib import Path
 def setup(app):
     file_dir = Path(__file__).parent
 
-    source_dir = file_dir.joinpath("../../adr").resolve()
+    source_dir = file_dir.joinpath("../../../adr").resolve()
     destination_dir = file_dir.joinpath("../adr/").resolve()
 
     for file in source_dir.glob("*.md"):
