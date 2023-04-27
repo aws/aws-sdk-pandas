569615f72b230b5db138eb4e86c6128c0db51ff5
Leon Luttenberger <luttenberger.leon@gmail.com>
2023-04-27 09:57:55 -0500

    fix to_csv

--- awswrangler/neptune/_neptune.py
+++ awswrangler/neptune/_neptune.py
@@ -279,7 +279,7 @@ def bulk_load(
     neptune_load_wait_polling_delay: float = 1.0,
     load_parallelism: Literal["LOW", "MEDIUM", "HIGH", "OVERSUBSCRIBE"] = "HIGH",
 ) -> None:
-    s3.to_csv(df, path, database=True)
+    s3.to_csv(df, path, dataset=True)
 
     load_id = client.load(
         path,
