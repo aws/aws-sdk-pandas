09c42a6b55bd2af03e83cfe4cbf03c6c598d68ad
Leon Luttenberger <LeonLuttenberger@users.noreply.github.com>
2023-05-03 11:47:12 -0500

    feat: Add `chunked` parameter to DynamoDB read functions (#2227)

--- tests/unit/test_dynamodb.py
+++ tests/unit/test_dynamodb.py
@@ -1,3 +1,4 @@
+import itertools
 import tempfile
 from datetime import datetime
 from decimal import Decimal
@@ -210,7 +211,8 @@ def test_dynamodb_put_from_file(
     ],
 )
 @pytest.mark.parametrize("use_threads", [False, True])
-def test_read_items_simple(params: Dict[str, Any], dynamodb_table: str, use_threads: bool) -> None:
+@pytest.mark.parametrize("chunked", [False, True])
+def test_read_items_simple(params: Dict[str, Any], dynamodb_table: str, use_threads: bool, chunked: bool) -> None:
     data = [
         {
             "par0": Decimal("2"),
@@ -244,12 +246,14 @@ def test_read_items_simple(params: Dict[str, Any], dynamodb_table: str, use_thre
     wr.dynamodb.put_df(df=df, table_name=dynamodb_table, use_threads=use_threads)
 
     with pytest.raises(wr.exceptions.InvalidArgumentCombination):
-        wr.dynamodb.read_items(table_name=dynamodb_table)
+        wr.dynamodb.read_items(table_name=dynamodb_table, use_threads=use_threads, chunked=chunked)
 
     with pytest.raises(wr.exceptions.InvalidArgumentType):
         wr.dynamodb.read_items(
             table_name=dynamodb_table,
             partition_values=[1],
+            use_threads=use_threads,
+            chunked=chunked,
         )
 
     with pytest.raises(wr.exceptions.InvalidArgumentCombination):
@@ -257,21 +261,50 @@ def test_read_items_simple(params: Dict[str, Any], dynamodb_table: str, use_thre
             table_name=dynamodb_table,
             partition_values=[1, 2],
             sort_values=["a"],
+            use_threads=use_threads,
+            chunked=chunked,
         )
 
     with pytest.raises((ClientError, AttributeError)):
-        wr.dynamodb.read_items(table_name=dynamodb_table, filter_expression="nonsense")
+        out = wr.dynamodb.read_items(
+            table_name=dynamodb_table, filter_expression="nonsense", use_threads=use_threads, chunked=chunked
+        )
+        if chunked:
+            next(out)
 
     df2 = wr.dynamodb.read_items(
-        table_name=dynamodb_table, max_items_evaluated=5, pyarrow_additional_kwargs={"types_mapper": None}
+        table_name=dynamodb_table,
+        max_items_evaluated=5,
+        pyarrow_additional_kwargs={"types_mapper": None},
+        use_threads=use_threads,
+        chunked=chunked,
     )
+    if chunked:
+        df2 = pd.concat(df2)
     assert df2.shape == df.shape
     assert df2.dtypes.to_list() == df.dtypes.to_list()
 
-    df3 = wr.dynamodb.read_items(table_name=dynamodb_table, partition_values=[2], sort_values=["b"])
+    df3 = wr.dynamodb.read_items(
+        table_name=dynamodb_table,
+        partition_values=[2],
+        sort_values=["b"],
+        use_threads=use_threads,
+        chunked=chunked,
+    )
+    if chunked:
+        df3 = pd.concat(df3)
     assert df3.shape == (1, len(df.columns))
 
-    assert wr.dynamodb.read_items(table_name=dynamodb_table, allow_full_scan=True, as_dataframe=False) == data
+    df4 = wr.dynamodb.read_items(
+        table_name=dynamodb_table,
+        allow_full_scan=True,
+        as_dataframe=False,
+        use_threads=use_threads,
+        chunked=chunked,
+    )
+    if chunked:
+        df4 = list(itertools.chain.from_iterable(df4))
+    assert df4 == data
 
 
 @pytest.mark.parametrize(
@@ -324,7 +357,8 @@ def test_read_items_reserved(params: Dict[str, Any], dynamodb_table: str, use_th
     ],
 )
 @pytest.mark.parametrize("use_threads", [False, True])
-def test_read_items_index(params: Dict[str, Any], dynamodb_table: str, use_threads: bool) -> None:
+@pytest.mark.parametrize("chunked", [False, True])
+def test_read_items_index(params: Dict[str, Any], dynamodb_table: str, use_threads: bool, chunked: bool) -> None:
     df = pd.DataFrame(
         {
             "Author": ["John Grisham", "John Grisham", "James Patterson"],
@@ -343,12 +377,17 @@ def test_read_items_index(params: Dict[str, Any], dynamodb_table: str, use_threa
         table_name=dynamodb_table,
         key_condition_expression=Key("Category").eq("Suspense"),
         index_name="CategoryIndex",
+        chunked=chunked,
     )
+    if chunked:
+        df2 = pd.concat(df2)
     assert df2.shape == df.shape
 
     df3 = wr.dynamodb.read_items(
-        table_name=dynamodb_table, allow_full_scan=True, index_name="CategoryIndex", use_threads=1
+        table_name=dynamodb_table, allow_full_scan=True, index_name="CategoryIndex", use_threads=1, chunked=chunked
     )
+    if chunked:
+        df3 = pd.concat(df3)
     assert df3.shape == df.shape
 
 
