136682b4862b3621ec7cf73e0b9f16243beb2dd5
Anton Kukushkin <kukushkin.anton@gmail.com>
2023-05-04 14:39:02 +0100

    Pandas 2.x - fix test util import & add dtype_backend rountrip test case for text formats

--- tests/_utils.py
+++ tests/_utils.py
@@ -13,7 +13,6 @@ import botocore.exceptions
 from packaging import version
 from pandas import DataFrame as PandasDataFrame
 from pandas import Series as PandasSeries
-from pandas.util.testing import assert_equal
 from pytest import FixtureRequest
 
 import awswrangler as wr
@@ -32,6 +31,11 @@ else:
 
     if version.parse(pd.__version__) >= version.parse("2.0.0"):
         is_pandas_2_x = True
+
+if is_pandas_2_x:
+    from pandas.testing import assert_frame_equal
+else:
+    from pandas.util.testing import assert_equal as assert_frame_equal
 
 
 CFN_VALID_STATUS = ["CREATE_COMPLETE", "ROLLBACK_COMPLETE", "UPDATE_COMPLETE", "UPDATE_ROLLBACK_COMPLETE"]
@@ -519,4 +523,4 @@ def pandas_equals(df1: Union[pd.DataFrame, pd.Series], df2: Union[pd.DataFrame, 
 
 def assert_pandas_equals(df1: Union[pd.DataFrame, pd.Series], df2: Union[pd.DataFrame, pd.Series]) -> None:
     df1, df2 = to_pandas(df1), to_pandas(df2)
-    assert_equal(df1, df2)
+    assert_frame_equal(df1, df2)
