e18ac7196a2da8fd5259b82a4a49a6fee8175532
Anton Kukushkin <kukushkin.anton@gmail.com>
2023-05-30 14:47:13 +0100

    [skip ci] Add job args typed dicts & update docstrings

--- awswrangler/emr_serverless.py
+++ awswrangler/emr_serverless.py
@@ -3,9 +3,10 @@
 import logging
 import pprint
 import time
-from typing import Any, Dict, List, Literal, Optional, Union
+from typing import Any, Dict, List, Literal, Optional, TypedDict, Union
 
 import boto3
+from typing_extensions import NotRequired, Required
 
 from awswrangler import _utils, exceptions
 from awswrangler._config import apply_configs
@@ -15,6 +16,28 @@ _logger: logging.Logger = logging.getLogger(__name__)
 
 _EMR_SERVERLESS_JOB_WAIT_POLLING_DELAY: float = 5  # SECONDS
 _EMR_SERVERLESS_JOB_FINAL_STATES: List[str] = ["SUCCESS", "FAILED", "CANCELLED"]
+
+
+class SparkSubmitJobArgs(TypedDict):
+    """Typed dictionary defining the Spark submit job arguments."""
+
+    entryPoint: Required[str]
+    """The entry point for the Spark submit job run."""
+    entryPointArguments: NotRequired[List[str]]
+    """The arguments for the Spark submit job run."""
+    sparkSubmitParameters: NotRequired[str]
+    """The parameters for the Spark submit job run."""
+
+
+class HiveRunJobArgs(TypedDict):
+    """Typed dictionary defining the Hive job run arguments."""
+
+    query: Required[str]
+    """The S3 location of the query file for the Hive job run."""
+    initQueryFile: NotRequired[str]
+    """The S3 location of the query file for the Hive job run."""
+    parameters: NotRequired[str]
+    """The parameters for the Hive job run."""
 
 
 @Experimental
@@ -42,19 +65,37 @@ def create_application(
     Parameters
     ----------
     name : str
+        Name of EMR Serverless appliation
     release_label : str
+        Release label e.g. `emr-6.10.0`
     application_type : str, optional
+        Application type: "Spark" or "Hive". Defaults to "Spark".
     initial_capacity : Dict[str, str], optional
+        The capacity to initialize when the application is created.
     maximum_capacity : Dict[str, str], optional
+        The maximum capacity to allocate when the application is created.
+        This is cumulative across all workers at any given point in time,
+        not just when an application is created. No new resources will
+        be created once any one of the defined limits is hit.
     tags : Dict[str, str], optional
+        Key/Value collection to put tags on the application.
+        e.g. {"foo": "boo", "bar": "xoo"})
     autostart : bool, optional
+        Enables the application to automatically start on job submission. Defaults to true.
     autostop : bool, optional
+        Enables the application to automatically stop after a certain amount of time being idle. Defaults to true.
     idle_timeout : int, optional
+        The amount of idle time in minutes after which your application will automatically stop. Defaults to 15 minutes.
     network_configuration : Dict[str, str], optional
+        The network configuration for customer VPC connectivity.
     architecture : str, optional
+        The CPU architecture of an application: "ARM64" or "X86_64". Defaults to "X86_64".
     image_uri : str, optional
+        The URI of an image in the Amazon ECR registry.
     worker_type_specifications : Dict[str, str], optional
+        The key-value pairs that specify worker type.
     boto3_session : boto3.Session(), optional
+        Boto3 Session. The default boto3 session will be used if boto3_session receive None.
 
     Returns
     -------
@@ -99,7 +140,7 @@ def create_application(
 def run_job(
     application_id: str,
     execution_role_arn: str,
-    job_driver_args: Dict[str, Any],
+    job_driver_args: Union[Dict[str, Any], SparkSubmitJobArgs, HiveRunJobArgs],
     job_type: Literal["Spark", "Hive"] = "Spark",
     wait: bool = True,
     configuration_overrides: Optional[Dict[str, Any]] = None,
@@ -117,16 +158,29 @@ def run_job(
     Parameters
     ----------
     application_id : str
+        The id of the application on which to run the job.
     execution_role_arn : str
-    job_driver_args : Dict[str, str]
+        The execution role ARN for the job run.
+    job_driver_args : Union[Dict[str, str], SparkSubmitJobArgs, HiveRunJobArgs]
+        The job driver arguments for the job run.
     job_type : str, optional
+        Type of the job: "Spark" or "Hive". Defaults to "Spark".
     wait : bool, optional
+        Whether to wait for the job completion or not. Defaults to true.
     configuration_overrides : Dict[str, str], optional
+        The configuration overrides for the job run.
     tags : Dict[str, str], optional
+        Key/Value collection to put tags on the application.
+        e.g. {"foo": "boo", "bar": "xoo"})
     execution_timeout : int, optional
+        The maximum duration for the job run to run. If the job run runs beyond this duration,
+        it will be automatically cancelled.
     name : str, optional
+        Name of the job.
     emr_serverless_job_wait_polling_delay : int, optional
+        Time to wait between polling attempts.
     boto3_session : boto3.Session(), optional
+        Boto3 Session. The default boto3 session will be used if boto3_session receive None.
 
     Returns
     -------
@@ -185,9 +239,13 @@ def wait_job(
     Parameters
     ----------
     application_id : str
+        The id of the application on which the job is running.
     job_run_id : str
+        The id of the job.
     emr_serverless_job_wait_polling_delay : int, optional
+        Time to wait between polling attempts.
     boto3_session : boto3.Session(), optional
+        Boto3 Session. The default boto3 session will be used if boto3_session receive None.
 
     Returns
     -------
