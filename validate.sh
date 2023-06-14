61f320f8b9fc60c2dd1a63fdbb289bf5fad83a40
Leon Luttenberger <LeonLuttenberger@users.noreply.github.com>
2023-06-14 12:22:30 -0600

    chore: Add static checking of poetry.lock consistency with pyproject.toml (#2342)

--- validate.sh
+++ validate.sh
@@ -7,3 +7,4 @@ ruff awswrangler
 mypy --install-types --non-interactive awswrangler
 pylint -j 0 --disable=all --enable=R0911,R0912,R0913,R0915 awswrangler
 doc8 --ignore-path docs/source/stubs --max-line-length 120 docs/source
+poetry lock --check
