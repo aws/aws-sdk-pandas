89a7367837104ccf0d14c7eebd716b1c433fb34e
kukushking <kukushkin.anton@gmail.com>
2023-11-13 16:15:56 +0000

    chore: Prepare 3.4.2 release (#2516)
    * Prepare 3.4.2 release
    
    Signed-off-by: Anton Kukushkin <kukushkin.anton@gmail.com>
    
    * Lambda layers - Update arrow to 14.0.1
    
    Signed-off-by: Anton Kukushkin <kukushkin.anton@gmail.com>
    
    ---------
    
    Signed-off-by: Anton Kukushkin <kukushkin.anton@gmail.com>

--- building/lambda/build-lambda-layer.sh
+++ building/lambda/build-lambda-layer.sh
@@ -16,7 +16,7 @@ export CMAKE_PREFIX_PATH=$ARROW_HOME:$CMAKE_PREFIX_PATH
 
 git clone \
   --depth 1 \
-  --branch apache-arrow-12.0.0 \
+  --branch apache-arrow-14.0.1 \
   --single-branch \
   https://github.com/apache/arrow.git
 
