feee7f14ee1c1de15736660f7e8e12548b1ae4c6
Leon Luttenberger <luttenberger.leon@gmail.com>
2023-12-03 15:44:46 +0100

    formatting fix

--- awswrangler/sqlserver.py
+++ awswrangler/sqlserver.py
@@ -64,7 +64,7 @@ def _drop_table(cursor: "Cursor", schema: Optional[str], table: str) -> None:
 
 def _does_table_exist(cursor: "Cursor", schema: Optional[str], table: str) -> bool:
     schema_str = f"TABLE_SCHEMA = '{schema}' AND" if schema else ""
-    cursor.execute(f"SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE " f"{schema_str} TABLE_NAME = ?", table)
+    cursor.execute(f"SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE {schema_str} TABLE_NAME = ?", table)
     return len(cursor.fetchall()) > 0
 
 
