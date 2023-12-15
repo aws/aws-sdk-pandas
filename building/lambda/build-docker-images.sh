2f05521d07b58f07dd762c95eb49425756d22094
Leon Luttenberger <luttenberger.leon@gmail.com>
2023-12-15 14:42:57 -0600

    test 3.12

--- building/lambda/build-docker-images.sh
+++ building/lambda/build-docker-images.sh
@@ -6,33 +6,33 @@ cp ../../poetry.lock .
 
 export DOCKER_BUILDKIT=1
 
-# Python 3.8
-docker build \
-  --pull \
-  --tag awswrangler-build-py38 \
-  --build-arg base_image=public.ecr.aws/lambda/python:3.8 \
-  .
+# # Python 3.8
+# docker build \
+#   --pull \
+#   --tag awswrangler-build-py38 \
+#   --build-arg base_image=public.ecr.aws/lambda/python:3.8 \
+#   .
 
-# Python 3.9
-docker build \
-  --pull \
-  --tag awswrangler-build-py39 \
-  --build-arg base_image=public.ecr.aws/lambda/python:3.9 \
-  .
+# # Python 3.9
+# docker build \
+#   --pull \
+#   --tag awswrangler-build-py39 \
+#   --build-arg base_image=public.ecr.aws/lambda/python:3.9 \
+#   .
 
-# Python 3.10
-docker build \
-  --pull \
-  --tag awswrangler-build-py310 \
-  --build-arg base_image=public.ecr.aws/lambda/python:3.10 \
-  .
+# # Python 3.10
+# docker build \
+#   --pull \
+#   --tag awswrangler-build-py310 \
+#   --build-arg base_image=public.ecr.aws/lambda/python:3.10 \
+#   .
 
-# Python 3.11
-docker build \
-  --pull \
-  --tag awswrangler-build-py311 \
-  --build-arg base_image=public.ecr.aws/lambda/python:3.11 \
-  .
+# # Python 3.11
+# docker build \
+#   --pull \
+#   --tag awswrangler-build-py311 \
+#   --build-arg base_image=public.ecr.aws/lambda/python:3.11 \
+#   .
 
 # Python 3.12
 docker build \
