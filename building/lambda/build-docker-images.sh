4de4fb33b1a79565ff3627c99e5990b2bfe555b7
Leon Luttenberger <luttenberger.leon@gmail.com>
2023-12-19 10:13:10 -0600

    rename Dockerfile

--- building/lambda/build-docker-images.sh
+++ building/lambda/build-docker-images.sh
@@ -39,7 +39,7 @@ docker build \
   --pull \
   --tag awswrangler-build-py312 \
   --build-arg base_image=public.ecr.aws/lambda/python:3.12 \
-  --file Dockerfile.py312 \
+  --file Dockerfile.al3 \
   .
 
 rm -rf pyproject.toml poetry.lock
