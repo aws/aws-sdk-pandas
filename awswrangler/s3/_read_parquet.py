6a31336150bf91cc7d897a3dffe3d7b78af247e9
Anton Kukushkin <kukushkin.anton@gmail.com>
2024-01-12 13:58:25 +0000

    fix: Preserve schema metadata
    Signed-off-by: Anton Kukushkin <kukushkin.anton@gmail.com>

--- awswrangler/s3/_read_parquet.py
+++ awswrangler/s3/_read_parquet.py
@@ -238,11 +238,9 @@ def _read_parquet_chunked(
                 batch_size=batch_size, columns=columns, use_threads=use_threads_flag, use_pandas_metadata=False
             )
 
-            if columns is not None:
-                field_dict = {field.name: field for field in pq_file.schema.to_arrow_schema()}
-                schema = pa.schema([field_dict[column] for column in columns])
-            else:
-                schema = pq_file.schema.to_arrow_schema()
+            schema = pq_file.schema.to_arrow_schema()
+            if columns:
+                schema = pa.schema([schema.field(column) for column in columns], schema.metadata)
 
             table = _add_table_partitions(
                 table=pa.Table.from_batches(chunks, schema=schema),
