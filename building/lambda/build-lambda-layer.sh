f8f68f31daf9d37516d57692edd7affd1b92fe55
Leon Luttenberger <luttenberger.leon@gmail.com>
2024-08-27 10:42:27 -0500

    override Numpy version in layer

--- building/lambda/build-lambda-layer.sh
+++ building/lambda/build-lambda-layer.sh
@@ -81,6 +81,9 @@ popd
 
 pushd /aws-sdk-pandas
 
+# Install Numpy 1.x because 2.x is not support in layers right now
+pip3 install "numpy==1.*" -t ./python
+
 pip3 install . -t ./python ".[redshift,mysql,postgres,gremlin,opensearch,openpyxl]"
 
 rm -rf python/pyarrow*
