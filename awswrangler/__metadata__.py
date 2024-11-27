517d28fd19cca85c168059a9bfc2429307fd8abc
Anton Kukushkin <kukushkin.anton@gmail.com>
2024-11-27 22:01:14 +0000

    chore: Prepare 3.10.1 release (#3031)

--- awswrangler/__metadata__.py
+++ awswrangler/__metadata__.py
@@ -7,5 +7,5 @@ Documentation: https://aws-sdk-pandas.readthedocs.io/
 
 __title__: str = "awswrangler"
 __description__: str = "Pandas on AWS."
-__version__: str = "3.10.0"
+__version__: str = "3.10.1"
 __license__: str = "Apache License 2.0"
