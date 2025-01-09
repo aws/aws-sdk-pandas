e7a52535539ca6208a8289c75355565e123c1ad1
kukushking <kukushkin.anton@gmail.com>
2025-01-09 14:51:58 +0000

    fix: add gcc10 to AL2 image; build numpy and pandas from source

--- building/lambda/build-lambda-layer.sh
+++ building/lambda/build-lambda-layer.sh
@@ -83,7 +83,7 @@ popd
 
 pushd /aws-sdk-pandas
 
-pip3 install . -t ./python ".[redshift,mysql,postgres,gremlin,opensearch,openpyxl]"
+pip3 install . --no-binary numpy,pandas -t ./python ".[redshift,mysql,postgres,gremlin,opensearch,openpyxl]"
 
 rm -rf python/pyarrow*
 rm -rf python/boto*
