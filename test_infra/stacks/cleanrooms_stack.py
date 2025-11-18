95849ac6b41fe1e4d82723ec34c174eaaebab88e
Anton Kukushkin <kukushkin.anton@gmail.com>
2025-11-18 17:50:22 +0000

    chore: update cleanrooms analytics engine (#3239)
    * chore: update cleanrooms analytics engine
    
    * typo

--- test_infra/stacks/cleanrooms_stack.py
+++ test_infra/stacks/cleanrooms_stack.py
@@ -25,6 +25,7 @@ class CleanRoomsStack(Stack):  # type: ignore
             description="Collaboration Room for AWS SDK for pandas test infrastructure",
             members=[],
             query_log_status="ENABLED",
+            analytics_engine="SPARK",
         )
 
         self.membership = cleanrooms.CfnMembership(
