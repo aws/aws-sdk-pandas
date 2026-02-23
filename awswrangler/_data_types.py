f3293c0d1971c080afa6604fe5d806c84a53ecfd
kukushking <kukushkin.anton@gmail.com>
2026-02-23 17:47:31 +0000

    fix: Remove unnecessary version resolution

--- awswrangler/_data_types.py
+++ awswrangler/_data_types.py
@@ -12,12 +12,9 @@ from typing import Any, Callable, Iterator, Match, Sequence
 import numpy as np
 import pandas as pd
 import pyarrow as pa
-from packaging import version
 
 from awswrangler import _arrow, exceptions
 from awswrangler._distributed import engine
-
-_PANDAS_GE_3 = version.parse(pd.__version__) >= version.parse("3.0.0")
 
 _logger: logging.Logger = logging.getLogger(__name__)
 
@@ -342,7 +339,7 @@ def athena2pyarrow(dtype: str, df_type: str | None = None) -> pa.DataType:  # no
         elif df_type == "datetime64[s]":
             return pa.timestamp(unit="s")
         else:
-            return pa.timestamp(unit="us" if _PANDAS_GE_3 else "ns")
+            return pa.timestamp(unit="ns")
     if dtype == "date":
         return pa.date32()
     if dtype in ("binary" or "varbinary"):
