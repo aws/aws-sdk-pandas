a166b689ceb73d5915ceb01e0fcf5db0b7ba6473
kukushking <kukushkin.anton@gmail.com>
2026-08-31 14:26:07 +0100

    fix: remove type ignores made unused by progressbar2 4.6 typing

--- awswrangler/opensearch/_write.py
+++ awswrangler/opensearch/_write.py
@@ -605,10 +605,10 @@ https://opendistro.github.io/for-elasticsearch-docs/docs/elasticsearch/rest-api-
     try:
         if progressbar:
             widgets = [
-                progressbar.Percentage(),  # type: ignore[no-untyped-call]
-                progressbar.SimpleProgress(format=" (%(value_s)s/%(max_value_s)s)"),  # type: ignore[no-untyped-call]
+                progressbar.Percentage(),
+                progressbar.SimpleProgress(format=" (%(value_s)s/%(max_value_s)s)"),
                 progressbar.Bar(),  # type: ignore[no-untyped-call]
-                progressbar.Timer(),  # type: ignore[no-untyped-call]
+                progressbar.Timer(),
             ]
             progress_bar = progressbar.ProgressBar(
                 widgets=widgets, max_value=total_documents, prefix="Indexing: "
